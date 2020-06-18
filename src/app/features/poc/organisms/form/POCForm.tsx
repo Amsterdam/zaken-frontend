c9776993848fb0af8eec23a9304e57b95349e0a5
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-06-18 11:40:00 +0200

    chore: pass create method to scaffold form

--- src/app/features/poc/organisms/form/POCForm.tsx
+++ src/app/features/poc/organisms/form/POCForm.tsx
@@ -5,20 +5,11 @@ import useGlobalActions from "../../globalstate/useGlobalActions"
 
 import scaffoldProps from "./scaffold"
 
-const mockHandleSubmit = (...args: any[]) => new Promise(resolve => {
-  console.log("Mock submit...", args)
-  // Resolve promise after a timeout.
-  setTimeout(() => {
-    console.log("...submitted!")
-    resolve()
-  }, 1000)
-})
-
 const POCForm: React.FC = () => {
   const { cases: { create } } = useGlobalActions()
 
   return (
-    <ScaffoldForm onSubmit={ (values: API.Case) => create(values) }>
+    <ScaffoldForm onSubmit={ create }>
       <Scaffold {...scaffoldProps} />
       <DebugFormValues />
     </ScaffoldForm>
