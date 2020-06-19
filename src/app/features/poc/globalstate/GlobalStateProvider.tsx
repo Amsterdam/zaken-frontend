01fb60d5750b132b49c0974cd98ece891e657d46
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-06-19 15:48:21 +0200

    Cases index

--- src/app/features/poc/globalstate/GlobalStateProvider.tsx
+++ src/app/features/poc/globalstate/GlobalStateProvider.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useEffect } from "react"
 import GlobalStateContext from "./GlobalStateContext"
 import { useRestActions } from "globalstate-hooks"
+import { api } from "./config"
 
 const GlobalStateProvider: React.FC = ({ children }) => {
-  const [cases, casesActions] = useRestActions<API.Case>({ api: { domain: "http://localhost:8000", name: "cases" } })
+  const [cases, casesActions] = useRestActions<API.Case>({ api: { ...api, name: "cases" } })
 
   const index = useCallback(casesActions.index, [])
   useEffect(() => { index() }, [index])
@@ -12,6 +13,7 @@ const GlobalStateProvider: React.FC = ({ children }) => {
     state: { cases },
     actions: { cases: casesActions }
   }
+
   return (
     <GlobalStateContext.Provider value={ value }>
       { children }
---
+++ src/app/features/poc/globalstate/config.ts
@@ -0,0 +1,4 @@
+export const api = {
+  domain: "http://localhost:8000",
+  path: "api/v1"
+}
