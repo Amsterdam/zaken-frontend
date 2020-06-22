019be5db64c5bea961c53f4fdac70de2c6cc3ad8
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-06-22 10:54:39 +0200

    Add test

--- src/app/features/shared/routing/to.test.ts
+++ src/app/features/shared/routing/to.test.ts
@@ -4,4 +4,9 @@ describe("to", () => {
   it("should should apply route params when given", () => {
     expect(to("/poc/foo/:bar/", { bar: "barValue" })).toEqual("/poc/foo/barValue/")
   })
+
+  it("should should not apply excess route params when given", () => {
+    // @ts-ignore
+    expect(to("/poc/foo/:bar/", { bar: "barValue", baz: "bazValue" })).toEqual("/poc/foo/barValue/")
+  })
 })
