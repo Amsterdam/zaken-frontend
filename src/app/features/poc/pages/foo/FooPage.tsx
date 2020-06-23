29671d9b231564b434863ccb35595197168f28e6
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-06-23 13:11:36 +0200

    feat: force to function to accept numbers and strings

--- src/app/features/poc/pages/foo/FooPage.tsx
+++ src/app/features/poc/pages/foo/FooPage.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { Heading } from "@datapunt/asc-ui"
 
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
-import { RouteComponentProps } from "@reach/router"
 
 type Props = {
   bar: string
@@ -10,7 +9,7 @@ type Props = {
 
 // NOTE: just a demo page to demonstrate route-params.
 // Please remove whenever there is a real feature introduced.
-const FooPage: React.FC<RouteComponentProps<Props>> = ({ bar }) => (
+const FooPage: React.FC<Props> = ({ bar }) => (
   <DefaultLayout>
     <Heading>Foo page</Heading>
     <p>Route param: { bar }</p>
