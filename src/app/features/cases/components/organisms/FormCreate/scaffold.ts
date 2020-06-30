37902f136a4de5244598d4f6631e2f2036cf8626
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-06-30 11:44:06 +0200

    chore: removed unnecessary fields

--- src/app/features/cases/components/organisms/FormCreate/scaffold.ts
+++ src/app/features/cases/components/organisms/FormCreate/scaffold.ts
@@ -4,20 +4,19 @@ import POSTCases from "__generated__/POST_Cases.json"
 
 import { Fields } from "app/features/shared/components/molecules/Form/ScaffoldFields"
 
-const fields = produce(POSTCases.fields, (draft) => {
-  draft.zaaktype.type = "CaseTypeField"
-})
+const fields = produce(POSTCases.fields, ({ startdatum, einddatum, zaaktype, omschrijving, submit }) => ({
+    startdatum,
+    einddatum,
+    zaaktype: { ...zaaktype, type: "CaseTypeField" },
+    omschrijving,
+    submit
+}))
 
 export default new FormPositioner(fields as Fields)
   .setVertical("mobileS")
-  .setGrid("laptop", "1fr 1fr", [
-    ["startdatum",                    "einddatum"],
-    ["url",                           "zaaktype"],
-    ["identificatie",                 "omschrijving"],
-    ["status",                        "omschrijving"],
-    ["uuid",                          "omschrijving"],
-    ["bronorganisatie",               "omschrijving"],
-    ["verantwoordelijkeOrganisatie",  "omschrijving"],
-    ["submit",                        "submit"]
+  .setGrid("laptop", "1fr 1fr 1fr", [
+    ["startdatum",    "einddatum",    "zaaktype"],
+    ["omschrijving",  "omschrijving", "omschrijving"],
+    ["submit",        "submit",       "submit"]
   ])
   .getScaffoldProps()
