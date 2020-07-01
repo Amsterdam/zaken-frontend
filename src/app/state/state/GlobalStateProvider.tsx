fb0d1663328bf6e4cfd2a380681df4e486498b70
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-07-01 09:44:44 +0200

    Add global state to window

--- src/app/state/state/GlobalStateProvider.tsx
+++ src/app/state/state/GlobalStateProvider.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { GlobalStateProvider } from "./globalState"
 import { useRestActions } from "globalstate-hooks"
 import { api } from "./config"
@@ -25,6 +25,11 @@ const Provider: React.FC = ({ children }) => {
       caseTypes: caseTypesActions
     }
   }
+
+  useEffect(() => {
+    if (!process.env.REACT_APP_DEBUG_MODE) return
+    (window as any).globalState = value.state
+  }, [value.state])
 
   return (
     <GlobalStateProvider value={ value }>
