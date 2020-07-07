be6e7f00a4a69650e35ff35b1fca70ed6062c7fd
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-07-07 11:13:36 +0200

    test: a little more info

--- .github/workflows/on-pull-request/start-backend.sh
+++ .github/workflows/on-pull-request/start-backend.sh
@@ -5,10 +5,27 @@ cd ./backend
 
 docker network create fixxx-looplijsten-backend_looplijsten_backend
 
+echo "----"
+echo "docker-compose up --build --detach"
+echo "----"
 docker-compose up --build --detach
 
-docker-compose run --rm openzaak.local python src/manage.py migrate
-docker-compose run --rm openzaak.local python src/manage.py createsuperuser --username ci --email ci\@example.com --no-input
+echo "----"
+echo "docker-compose run --rm openzaak.test python src/manage.py migrate"
+echo "----"
+docker-compose run --rm openzaak.test python src/manage.py migrate
 
+echo "----"
+echo "docker-compose run --rm openzaak.test python src/manage.py createsuperuser --username ci --email ci\@example.com --no-input"
+echo "----"
+docker-compose run --rm openzaak.test python src/manage.py createsuperuser --username ci --email ci\@example.com --no-input
+
+echo "----"
+echo "docker-compose exec -T db psql -U postgres -d postgres -c \"INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties)  VALUES ('3223d651-827c-47f2-aff1-da6f016e072b', '{client}', 'Dev client', 't')\""
+echo "----"
 docker-compose exec -T db psql -U postgres -d postgres -c "INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties)  VALUES ('3223d651-827c-47f2-aff1-da6f016e072b', '{client}', 'Dev client', 't')"
+
+echo "----"
+echo "docker-compose exec -T db psql -U postgres -d postgres -c \"INSERT INTO vng_api_common_jwtsecret (id, identifier, secret)  VALUES (1, 'client', 'secret_key')\""
+echo "----"
 docker-compose exec -T db psql -U postgres -d postgres -c "INSERT INTO vng_api_common_jwtsecret (id, identifier, secret)  VALUES (1, 'client', 'secret_key')"
