84f4e0acaa12aa29f3156a174cd8e59e6fdda5ae
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-07-13 16:19:52 +0200

    Only allow for logging to Sentry:acceptance

--- src/app/config/environment.ts
+++
@@ -1,7 +0,0 @@
-// TODO Get environment from .env file
-const { hostname } = window.location
-const environment =
-  hostname === process.env.REACT_APP_DOMAIN_PRODUCTION ? "production" :
-  hostname === process.env.REACT_APP_DOMAIN_ACCEPTANCE ? "acceptance" :
-  undefined
-export default environment
--- src/app/sentry/init.ts
+++ src/app/sentry/init.ts
@@ -1,10 +1,10 @@
 import * as Sentry from "@sentry/browser"
-import environment from "../config/environment"
 
 export default () => {
-  if (environment === undefined) return
-
   const dsn = process.env.REACT_APP_SENTRY_DSN
+  const environment = process.env.REACT_APP_SENTRY_ENVIRONMENT
+
+  if (dsn === undefined || environment === undefined) return
 
   // TODO Pass Git commit hash as third argument
   Sentry.init({
