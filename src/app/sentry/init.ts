a1120c645308471549996348da9e8738aaebd5f3
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-07-13 14:32:24 +0200

    Remove console.log

--- src/app/sentry/init.ts
+++ src/app/sentry/init.ts
@@ -2,7 +2,6 @@ import * as Sentry from "@sentry/browser"
 import environment from "../config/environment"
 
 export default () => {
-  console.log(environment)
   if (environment === undefined) return
 
   const dsn = process.env.REACT_APP_SENTRY_DSN
