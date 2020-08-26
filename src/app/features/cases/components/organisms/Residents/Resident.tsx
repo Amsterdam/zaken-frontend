eb048fff0ea591567fed13819d5ad821332dc7f0
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-08-26 13:23:22 +0200

    Improve Resident

--- src/app/features/cases/components/organisms/Residents/Resident.tsx
+++ src/app/features/cases/components/organisms/Residents/Resident.tsx
@@ -3,6 +3,7 @@ import Details from "app/features/shared/components/molecules/Details/Details"
 
 type Props = {
   resident: API.Resident
+  num: number
 }
 
 const mapSex = (value: API.Resident["geslachtsaanduiding"]) => {
@@ -11,16 +12,17 @@ const mapSex = (value: API.Resident["geslachtsaanduiding"]) => {
   if (value === "X") return ""
 }
 
-const Resident: React.FC<Props> = ({ resident }) => {
+const Resident: React.FC<Props> = ({ resident, num }) => {
   const values = useMemo(() => ({
-    "Naam": `${ mapSex(resident.geslachtsaanduiding) }${ resident.voorletters }. ${ resident.voorvoegsel_geslachtsnaam } ${ resident.geslachtsnaam }`,
+    [`${ num }. ${ mapSex(resident.geslachtsaanduiding) }${ resident.voorletters }. ${ resident.voorvoegsel_geslachtsnaam } ${ resident.geslachtsnaam }`]: "",
     "Geboren": resident.geboortedatum.substr(0, 10),
     "Ingeschreven sinds": resident.datum_begin_relatie_verblijadres.substr(0, 10),
-    "Uitgeschreven sinds": "__TODO__"
-  }), [resident])
+    "Uitgeschreven sinds": undefined
+  }), [num, resident.datum_begin_relatie_verblijadres, resident.geboortedatum, resident.geslachtsaanduiding, resident.geslachtsnaam, resident.voorletters, resident.voorvoegsel_geslachtsnaam])
   return <Details
-    numInitialVisibleRows={3}
+    numInitialVisibleRows={4}
     values={ values }
+    startAlternative={ false }
   />
 }
 export default Resident
