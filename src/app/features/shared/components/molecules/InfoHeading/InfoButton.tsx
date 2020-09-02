2800a62700741d38447dd44551e32085525aebe7
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-09-02 10:48:35 +0200

    Remove need for styling Icon

--- src/app/features/shared/components/molecules/InfoHeading/InfoButton.tsx
+++ src/app/features/shared/components/molecules/InfoHeading/InfoButton.tsx
@@ -1,20 +1,15 @@
 import React, { ComponentProps, FC } from "react"
-import styled from "styled-components"
 import { Button, Icon } from "@datapunt/asc-ui"
 
 type Props = {
   onClick?: ComponentProps<typeof Button>["onClick"]
 }
 
-const StyledIcon = styled(Icon)`
-  background-size: contain;
-`
-
 const InfoButton: FC<Props> = ({ onClick = () => {} }) =>
   <Button
     variant="blank"
     onClick={ onClick }
     // TODO: Use Info icon from ASC. Once https://github.com/Amsterdam/amsterdam-styled-components/pull/1122 has been merged.
-    icon={ <StyledIcon size={ 20 } iconUrl={ `${ process.env.PUBLIC_URL }/icons/Info.svg` } /> }
+    icon={ <Icon size={ 20 } iconUrl={ `${ process.env.PUBLIC_URL }/icons/Info.svg` } /> }
   />
 export default InfoButton
