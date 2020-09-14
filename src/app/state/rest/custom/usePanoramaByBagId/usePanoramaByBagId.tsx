e3f0c45e099388048c21d55630dee7a115f28f8f
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-09-14 10:37:45 +0200

    chore give panorama preview a width property

--- src/app/state/rest/custom/usePanoramaByBagId/usePanoramaByBagId.tsx
+++ src/app/state/rest/custom/usePanoramaByBagId/usePanoramaByBagId.tsx
@@ -1,11 +1,12 @@
 import { useBAG, usePanorama } from "../../index"
 
-const usePanoramaByBagId = (bagId: string) => {
+const usePanoramaByBagId = (bagId: string, width: number) => {
   const { data } = useBAG(bagId)
 
   return usePanorama(
     data?.results?.[0].centroid[1],
     data?.results?.[0].centroid[0],
+    width,
     { lazy: data === undefined }
   )
 }
