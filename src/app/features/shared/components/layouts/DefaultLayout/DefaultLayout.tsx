df3e7b695974b321d64a325c09691c6f2b84c546
Remco Hoff <remco@code010.nl>
2020-09-15 10:00:49 +0200

    Added left-aligned navigation

--- src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,19 +1,49 @@
 import React from "react"
-import { Header } from "@datapunt/asc-ui"
-
+import { Header, MenuInline, MenuItem, MenuButton, MenuFlyOut, SearchBar } from "@datapunt/asc-ui"
+import styled from "styled-components"
 import DefaultNavigation from "app/features/shared/components/organisms/navigation/DefaultNavigation"
 import { MainWrapper } from "app/features/shared/components/atoms/MainWrapper/MainWrapper"
 import to from "app/features/shared/routing/to"
 import FlashMessages from "app/features/shared/components/molecules/FlashMessages/FlashMessages"
 
+
+const LeftAlignWrap = styled.div`
+  margin-right: auto;
+`
+
+
 const DefaultLayout: React.FC = ({ children }) => (
   <>
-    <Header
-      title="Zaken"
-      homeLink={to("/")}
-      fullWidth={true}
-      navigation={<DefaultNavigation />}
-    />
+    <Header 
+      tall 
+      title="Zaken" 
+      homeLink={to("/")} 
+      navigation={
+        <LeftAlignWrap>
+        <MenuInline >
+          <MenuItem>
+            <MenuButton forwardedAs="a" href="/" active>
+              Menu item
+            </MenuButton>
+          </MenuItem>
+          <MenuFlyOut label="Menu item 2">
+            <MenuItem>
+              <MenuButton forwardedAs="a" href="/" active>
+                Submenu item
+              </MenuButton>
+            </MenuItem>
+          </MenuFlyOut>
+        </MenuInline>
+        </LeftAlignWrap>}
+        />
+
+<span>haai</span>
+        <SearchBar
+        placeholder="Enter the search text"
+        onChange={() => {
+          
+        }}
+      />
     <MainWrapper>
       <FlashMessages />
       { children }
