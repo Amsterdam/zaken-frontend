198048349dd50655f2927bdb89c8f0994e5c4301
Remco Hoff <remco@code010.nl>
2020-09-15 13:13:33 +0200

    cleanup code, undo Grip bypass

--- src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,18 +1,16 @@
 import React from "react"
-import { Header, MenuInline, MenuItem, MenuButton, MenuFlyOut, SearchBar } from "@datapunt/asc-ui"
+import { Header } from "@datapunt/asc-ui"
 import styled from "styled-components"
 import DefaultNavigation from "app/features/shared/components/organisms/navigation/DefaultNavigation"
 import { MainWrapper } from "app/features/shared/components/atoms/MainWrapper/MainWrapper"
 import to from "app/features/shared/routing/to"
 import FlashMessages from "app/features/shared/components/molecules/FlashMessages/FlashMessages"
 
-
 const MenuWithSearchButtonWrap = styled.div`
   margin-right: auto;
   position: relative;
   width: 100%;
 `
-
 
 const DefaultLayout: React.FC = ({ children }) => (
   <>
