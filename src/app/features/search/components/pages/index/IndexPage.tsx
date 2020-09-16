b2049a6adc6862aecafb8f3a854dcae7cd9dd162
Remco Hoff <remco@code010.nl>
2020-09-16 17:33:57 +0200

    Throttling the sendQuery

--- src/app/features/search/components/pages/index/IndexPage.tsx
+++ src/app/features/search/components/pages/index/IndexPage.tsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react"
+import React, { useState, useCallback } from "react"
 import { RouteComponentProps } from "@reach/router"
 import { SearchBar } from "@datapunt/asc-ui"
+import _ from "lodash"
 
 import Heading from "app/features/shared/components/atoms/Heading/Heading"
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
-
 import SearchResults from "app/features/search/components/organisms/SearchResults/SearchResults"
 
 const IndexPage: React.FC<RouteComponentProps> = () => {
   const [searchString, setSearchString] = useState("")
+  const delayedQuery = useCallback(_.debounce(( q: any ) => sendQuery(q), 750), [])
+  const sendQuery = (query: any) => setSearchString(query)
 
   const handleChange = (event: any) => {
     let searchString: string = event.target.value
-    searchString.length > 2 && setSearchString(searchString)
+    searchString.length > 2 && delayedQuery(searchString)
   }
 
   type Props = {
@@ -21,7 +23,7 @@ const IndexPage: React.FC<RouteComponentProps> = () => {
 
   return (
     <DefaultLayout>
-      // TODO: Add SearchForm, remove Heading
+      // TODO: Remove Heading
       <Heading>Zoeken</Heading>
       <SearchBar 
               placeholder="Zoek op postcode/huisnummer of straatnaam"
