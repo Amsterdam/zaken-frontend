11898b0e9eccfb09d9b7cc1480f7f8fff99ef2fc
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-09-17 15:07:44 +0200

    Add possibility for subroutes

--- src/app/config/utils/routesToRouteConfig.ts
+++ src/app/config/utils/routesToRouteConfig.ts
@@ -1,13 +1,28 @@
 import { FC } from "react"
 import type { RouteComponentProps } from "@reach/router"
+import slashSandwich from "slash-sandwich"
+
 type Page = FC<RouteComponentProps>
-type RouteConfig = { publicly: boolean, Page: Page }
+type RouteConfig = {
+  Page: Page
+  publicly?: boolean
+  subRoutes?: Record<string, RouteConfig | Page>
+}
 
 const toRouteConfig = (config: RouteConfig | Page): RouteConfig =>
-  "Page" in config ? config : { publicly: false, Page: config }
+  "Page" in config ?
+    { publicly: false, ...config } :
+    { publicly: false, Page: config }
 
 export default (routes: Record<string, RouteConfig | Page>) =>
   Object.keys(routes).reduce((acc, key) => {
-    acc[key] = toRouteConfig(routes[key])
+    const route = routes[key]
+    const routeConfig = toRouteConfig(route)
+    acc[key] = routeConfig
+    Object.keys(routeConfig.subRoutes ?? {}).forEach(subKey => {
+      const config = routeConfig.subRoutes?.[subKey]
+      if (config === undefined) return
+      acc[slashSandwich([key, subKey])] = toRouteConfig(config)
+    })
     return acc
   }, {} as Record<string, RouteConfig>)
