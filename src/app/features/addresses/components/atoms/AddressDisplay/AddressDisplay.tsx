1614d15a0cb6ed9615153f4a9c4025e5281d58e8
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-09-17 11:28:18 +0200

    feat: other address switcher

--- src/app/features/addresses/components/atoms/AddressDisplay/AddressDisplay.tsx
+++ src/app/features/addresses/components/atoms/AddressDisplay/AddressDisplay.tsx
@@ -1,14 +1,29 @@
 import React from "react"
 import { useBAG } from "app/state/rest"
 import { Heading } from "@datapunt/asc-ui"
+import styled from "styled-components"
+
+import ShowOtherAddressesButton from "app/features/addresses/components/organisms/AddressSuffixSwitcher/ShowOtherAddressesButton"
+import useOtherAddressesByBagId from "app/state/rest/custom/useOtherAddresses/useOtherAddresses"
 
 type Props = {
   bagId: string
 }
 
+const StyledHeading = styled(Heading)`
+  display: block;
+`
+
 const AddressDisplay: React.FC<Props> = ({ bagId }) => {
   const { data } = useBAG(bagId)
+  const { data: otherAddresses } = useOtherAddressesByBagId(bagId)
+
   // TODO: Show loading status visually
-  return <Heading>{ data ? `${ data.results[0].adres }, ${ data.results[0].postcode }` : "" }</Heading>
+  return <>
+    <StyledHeading>
+      { data ? `${ data.results[0].adres }, ${ data.results[0].postcode }` : "" }
+      { otherAddresses?.results && otherAddresses?.results?.length > 1 && <ShowOtherAddressesButton bagId={bagId} /> }
+    </StyledHeading>
+  </>
 }
 export default AddressDisplay
