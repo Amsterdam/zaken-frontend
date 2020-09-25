7a7df5d7a563b7938c77430dc7a4c07c82b5a0d7
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-09-25 11:05:57 +0200

    Addresses Cases

--- src/app/features/addresses/routes.tsx
+++ src/app/features/addresses/routes.tsx
@@ -1,6 +1,7 @@
 import IndexPage from "./components/pages/index/IndexPage"
 import DetailsPage from "./components/pages/details/DetailsPage"
 import PeoplePage from "./components/pages/people/PeoplePage"
+import CasesPage from "./components/pages/cases/CasesPage"
 import PermitIndexPage from "app/features/permits/components/pages/index/IndexPage"
 
 // NOTE: please add your own POC-specific routes here.
@@ -11,7 +12,7 @@ export default {
       "details": DetailsPage,
       "personen": PeoplePage,
       "vergunningen": PermitIndexPage,
-      "zaken": DetailsPage
+      "zaken": CasesPage
     }
   }
 }
