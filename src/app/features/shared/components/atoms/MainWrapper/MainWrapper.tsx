84c3c3a88e258fe704ab10b6e1d0e03dcadc7c00
Remco Hoff <remco@code010.nl>
2020-09-29 11:48:24 +0200

    Less margin on mobile

--- src/app/features/shared/components/atoms/MainWrapper/MainWrapper.tsx
+++ src/app/features/shared/components/atoms/MainWrapper/MainWrapper.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components"
-import { themeSpacing } from "@datapunt/asc-ui"
+import { breakpoint, themeSpacing } from "@datapunt/asc-ui"
 
-// TODO less margin on mobile.
 // TODO introduce a Spacing component?
 export const MainWrapper = styled.div`   
-  margin: ${ themeSpacing(10) };
+  margin: ${ themeSpacing(3) };
+  @media screen and ${ breakpoint("min-width", "laptop") } {
+    margin: ${ themeSpacing(10) };
+  }
 `
