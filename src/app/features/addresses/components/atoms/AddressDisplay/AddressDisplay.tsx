011502ca45fe289eeccb04e468caf03e5455d21a
Remco Hoff <remco@code010.nl>
2020-10-01 10:56:15 +0200

    Styling span as H1 / H2

--- src/app/features/addresses/components/atoms/AddressDisplay/AddressDisplay.tsx
+++ src/app/features/addresses/components/atoms/AddressDisplay/AddressDisplay.tsx
@@ -1,14 +1,19 @@
 import React from "react"
 import { useBAG } from "app/state/rest"
-import { Typography } from "@datapunt/asc-ui"
+import { themeSpacing, Typography } from "@datapunt/asc-ui"
 
 import ShowOtherAddressesButton from "app/features/addresses/components/organisms/AddressSuffixSwitcher/ShowOtherAddressesButton"
 import useOtherAddressesByBagId from "app/state/rest/custom/useOtherAddresses/useOtherAddresses"
+import styled from "styled-components"
 
 type Props = {
   bagId: string
   headingSize?: "h1" | "h2"
 }
+
+const StyledDiv = styled.div`
+  margin-top: ${ themeSpacing(1) }
+`
 
 
 const AddressDisplay: React.FC<Props> = ({ bagId, headingSize = "h2" }) => {
@@ -16,11 +21,13 @@ const AddressDisplay: React.FC<Props> = ({ bagId, headingSize = "h2" }) => {
   const { data: otherAddresses } = useOtherAddressesByBagId(bagId)
 
   // TODO: Show loading status visually
-  return <>
-    <Typography as="span" styleAs={ headingSize }>
-    { data ? `${ data.results[0].adres }, ${ data.results[0].postcode }` : "" }
-          { otherAddresses?.results && otherAddresses?.results?.length > 1 && <ShowOtherAddressesButton bagId={bagId} /> }
-    </Typography>
-  </>
+  return (
+    <StyledDiv>
+      <Typography as="span" styleAs={ headingSize }>
+      { data ? `${ data.results[0].adres }, ${ data.results[0].postcode }` : "" }
+            { otherAddresses?.results && otherAddresses?.results?.length > 1 && <ShowOtherAddressesButton bagId={bagId} /> }
+      </Typography>
+    </StyledDiv>
+  )
 }
 export default AddressDisplay
