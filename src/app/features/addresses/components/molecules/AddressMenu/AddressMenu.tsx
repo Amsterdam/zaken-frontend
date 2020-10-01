f5b43b70aea500f2bf4c9a0c1e225c2067c40164
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-10-01 12:12:09 +0200

    Make Nav blocks responsive

--- src/app/features/addresses/components/molecules/AddressMenu/AddressMenu.tsx
+++ src/app/features/addresses/components/molecules/AddressMenu/AddressMenu.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { themeSpacing } from "@datapunt/asc-ui"
+import { themeSpacing, breakpoint } from "@datapunt/asc-ui"
 import NavBlock from "app/features/addresses/components/atoms/NavBlock/NavBlock"
 import to from "app/features/shared/routing/to"
 import routesObject from "app/config/routes"
@@ -8,23 +8,32 @@ import routesObject from "app/config/routes"
 type Props = {
   bagId: Components.Schemas.Address["bag_id"]
 }
-
 const Menu = styled.menu`
   width: 100%;
-  margin:  ${ themeSpacing(4) } 0;
+  margin: 0;
   padding: 0;
 `
 const Ul = styled.ul`
-  margin: 0;
+  margin: 0 -${ themeSpacing(1.5) };
   padding: 0;
   list-style: none;
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
-
-  li {
-    margin-bottom: ${ themeSpacing(4) };
+`
+const Li = styled.li`
+  display: inline-block;
+  width: 100%;
+  margin-bottom: ${ themeSpacing(3) };
+  @media screen and ${ breakpoint("min-width", "tabletS") } {
+    width: 50%;
   }
+  @media screen and ${ breakpoint("min-width", "laptop") } {
+    width: 25%;
+  }
+`
+const Div = styled.div`
+  margin: 0 ${ themeSpacing(1.5) };
 `
 
 const routes = [
@@ -42,9 +51,11 @@ const AddressMenu: React.FC<Props> = ({ bagId }) =>
           const page = routesObject[route]
           if (page?.icon === undefined || page?.title === undefined) return null
           return (
-            <li key={ route }>
-              <NavBlock to={ to(route, { bagId }) } icon={ page.icon } header={ page.title } />
-            </li>
+            <Li key={ route }>
+              <Div>
+                <NavBlock to={ to(route, { bagId }) } icon={ page.icon } header={ page.title } />
+              </Div>
+            </Li>
           )
         })
       }
