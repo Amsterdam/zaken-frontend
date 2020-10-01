141614e42d413db7442b611168232bca3a2a7637
Remco Hoff <remco@code010.nl>
2020-10-01 16:12:42 +0200

    Added heading with icon

--- src/app/features/addresses/components/pages/details/DetailsPage.tsx
+++ src/app/features/addresses/components/pages/details/DetailsPage.tsx
@@ -1,36 +1,37 @@
 import React from "react"
 import { RouteComponentProps } from "@reach/router"
-import { themeSpacing } from "@datapunt/asc-ui"
+import {   themeSpacing } from "@datapunt/asc-ui"
 
+
+import routesObject from "app/config/routes"
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
-import PanoramaPreview from "app/features/cases/components/organisms/Panorama/PanoramaPreview"
-import BagMap from "app/features/cases/components/organisms/BagMap/BagMap"
 import BagDetails from "app/features/cases/components/organisms/BagDetails/BagDetails"
-import Row, { RowWithColumn } from "app/features/shared/components/atoms/Grid/Row"
-import Column from "app/features/shared/components/atoms/Grid/Column"
+import { RowWithColumn } from "app/features/shared/components/atoms/Grid/Row"
 import DetailHeader from "app/features/shared/components/organisms/DetailHeader/DetailHeader"
+import HeadingWithIcon from "app/features/shared/components/organisms/HeadingWithIcon/HeadingWithIcon"
 
 type Props = {
   bagId: Components.Schemas.Address["bag_id"]
 }
 
-const DetailPage: React.FC<RouteComponentProps<Props>> = ({ bagId }) => (
+const route = "/adres/:bagId/detail/"
+
+const DetailPage: React.FC<RouteComponentProps<Props>> = ({ bagId }) => {
+  const page = routesObject[route]
+  
+  return (
     <DefaultLayout>
-      <RowWithColumn marginBottom={ themeSpacing(10) }>
+      <RowWithColumn marginBottom={ themeSpacing(2) }>
         <DetailHeader bagId={ bagId! } />
       </RowWithColumn>
-      <Row>
-        <Column spanSmall={100} spanLarge={40}>
-          <PanoramaPreview bagId={ bagId! } />
-          </Column>
-        <Column spanSmall={100} spanLarge={60}>
-          <BagMap bagId={ bagId! } />
-        </Column>
-      </Row>
+      <RowWithColumn>
+        <HeadingWithIcon icon={ page?.icon ?? "ChevronRight" } header={ page?.title ?? "" } />
+      </RowWithColumn>
       <RowWithColumn>
         <BagDetails bagId={ bagId ! } />
       </RowWithColumn>
     </DefaultLayout>
   )
+}
 
 export default DetailPage
