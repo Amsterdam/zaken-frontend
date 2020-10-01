ee6036b3b3f4e91bff23f7438aeab94edc854134
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-10-01 16:36:03 +0200

    Add TODOs

--- src/app/features/addresses/components/organisms/People/People.tsx
+++ src/app/features/addresses/components/organisms/People/People.tsx
@@ -5,5 +5,6 @@ type Props = {
   bagId: Components.Schemas.Address["bag_id"]
 }
 
-const People: React.FC<Props> = ({ bagId }) => <Residents id="297951_7" />
+// TODO: Do show Residents by BAG_id
+const People: React.FC<Props> = () => <Residents id="297951_7" />
 export default People
