308f2354be6e369681e43b97f6d391ca4eb4fd74
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-10-05 12:17:37 +0200

    Hook useRect

--- src/app/features/cases/components/organisms/Panorama/PanoramaPreview.tsx
+++ src/app/features/cases/components/organisms/Panorama/PanoramaPreview.tsx
@@ -3,11 +3,12 @@ import styled from "styled-components"
 import { themeColor } from "@datapunt/asc-ui"
 
 import usePanoramaByBagId from "app/state/rest/custom/usePanoramaByBagId/usePanoramaByBagId"
+import useRect from "./hooks/useRect"
 
 type Props = {
   bagId: Components.Schemas.Address["bag_id"]
   width?: number
-  height?: number
+  aspect?: number
   radius?: number
 }
 
@@ -15,21 +16,20 @@ const Div = styled.div`
   background-color: ${ themeColor("tint", "level2") };
 `
 const Img = styled.img`
-  max-width: 100%;
+  width: 100%;
 `
 
-// TODO: https://codesandbox.io/s/userect-hook-1y5t7?file=/src/useRect.tsx
-
-const PanoramaPreview: React.FC<Props> = ({ bagId, width: w, height, radius = 180 }) => {
+const PanoramaPreview: React.FC<Props> = ({ bagId, width: w, aspect = 1.5, radius = 180 }) => {
   const ref = useRef<HTMLDivElement>(null)
-  const width = w ?? ref.current?.getBoundingClientRect().width
-  const aspect = width && height ? width / height : 1.5 // default aspect
+  const rect = useRect(ref, 100)
+  const width = w ?? rect.width
+  const height = width !== undefined ? width / aspect : undefined
   const { data } = usePanoramaByBagId(bagId, width, aspect, radius)
 
   return (
     <Div ref={ ref } style={ { height } }>
       { data ?
-        <Img width={ width } src={ data.url } alt={ `Panorama preview voor BAG: ${ bagId }` } /> :
+        <Img src={ data.url } alt={ `Panorama preview voor BAG: ${ bagId }` } /> :
         null
       }
     </Div>
