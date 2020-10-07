49329143b5a19694073da72854766ae4cca49d83
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-10-07 14:54:27 +0200

    Mobile Address header

--- src/app/features/addresses/components/atoms/AddressDisplay/AddressDisplay.tsx
+++ src/app/features/addresses/components/atoms/AddressDisplay/AddressDisplay.tsx
@@ -1,7 +1,8 @@
 import React from "react"
-import { useBAG } from "app/state/rest"
-import { Typography } from "@datapunt/asc-ui"
+import styled from "styled-components"
+import { Typography, breakpoint, themeSpacing } from "@datapunt/asc-ui"
 
+import { useBAG } from "app/state/rest"
 import ShowOtherAddressesButton from "app/features/addresses/components/organisms/AddressSuffixSwitcher/ShowOtherAddressesButton"
 import useOtherAddressesByBagId from "app/state/rest/custom/useOtherAddresses/useOtherAddresses"
 
@@ -11,22 +12,51 @@ type Props = {
   isHeader?: boolean
 }
 
+const Div = styled.div<{ isHeader: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  @media screen and ${ breakpoint("min-width", "laptop") } {
+    justify-content: ${ props => props.isHeader ? "flex-start" : "flex-end" };
+  }
+`
+
+const TypographyWrap = styled.div`
+  span {
+    margin-bottom: 0;
+  }
+`
+
+const ButtonWrap = styled.div`
+  margin-left: ${ themeSpacing(3) };
+`
+
 const AddressDisplay: React.FC<Props> = ({ bagId, headingSize = "h2", isHeader = false }) => {
   const { data } = useBAG(bagId)
   const { data: otherAddresses } = useOtherAddressesByBagId(bagId)
 
-  const isCurrentAddress = (address: any) => address.adres.trim() === data?.results[0].adres.trim()
+  const title = data ? `${ data.results[0].adres }, ${ data.results[0].postcode }` : undefined
+  const showTitle = title !== undefined
+
+  const showButton = otherAddresses?.results !== undefined && otherAddresses.results.length > 1
+  const isCurrentAddress = (address: { adres: string }) => address.adres.trim() === data?.results[0].adres.trim()
   const addressIndex = otherAddresses?.results.findIndex(isCurrentAddress)
   const index = addressIndex === 0 ? "first" : addressIndex === (otherAddresses?.results.length ! - 1) ? "last" : undefined
 
   // TODO: Show loading status visually
   return (
-    <Typography as={ isHeader ? headingSize : "span" } styleAs={ headingSize }>
-    { data ? `${ data.results[0].adres }, ${ data.results[0].postcode }` : "" }
-          { otherAddresses?.results && otherAddresses?.results?.length > 1 &&
-            <ShowOtherAddressesButton bagId={bagId} index={index} />
-          }
-    </Typography>
+    <Div isHeader={ isHeader }>
+      { showTitle &&
+        <TypographyWrap>
+          <Typography as={ isHeader ? headingSize : "span" } styleAs={ headingSize }>{ title }</Typography>
+        </TypographyWrap>
+      }
+      { showButton &&
+        <ButtonWrap>
+          <ShowOtherAddressesButton bagId={bagId} index={index} />
+        </ButtonWrap>
+      }
+    </Div>
   )
 }
 export default AddressDisplay
