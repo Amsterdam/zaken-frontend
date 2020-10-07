02f9e86291883861ddb1c1094280ad2dedfddcb5
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-10-07 15:26:18 +0200

    Remove ColumnAlignRight

--- src/app/features/shared/components/organisms/DetailHeader/DetailHeader.tsx
+++ src/app/features/shared/components/organisms/DetailHeader/DetailHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import styled from "styled-components"
-import { breakpoint } from "@datapunt/asc-ui"
+
+
 import Row from "app/features/shared/components/atoms/Grid/Row"
 import Column from "app/features/shared/components/atoms/Grid/Column"
 import BreadCrumbs from "app/features/addresses/components/molecules/BreadCrumbs/BreadCrumbs"
@@ -10,20 +10,14 @@ type Props = {
   bagId: string
 }
 
-const ColumnAlignRight = styled(Column)`
-@media screen and ${ breakpoint("min-width", "laptop") } {
-  text-align: right;
-}`
-
-
 const DetailHeader: React.FC<Props> = ({ bagId }) => (
   <Row>
     <Column spanLarge={50}>
       <BreadCrumbs bagId={ bagId! } />
     </Column>
-    <ColumnAlignRight spanLarge={50}>
+    <Column spanLarge={50}>
       <AddressHeader bagId={ bagId! } />
-    </ColumnAlignRight>
+    </Column>
   </Row>
 )
 
