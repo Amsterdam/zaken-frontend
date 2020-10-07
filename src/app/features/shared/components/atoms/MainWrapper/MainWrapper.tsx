cbae747284122359c386048615ee219a4bb50a19
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-10-07 11:37:47 +0200

    Page layout

--- src/app/features/shared/components/atoms/MainWrapper/MainWrapper.tsx
+++ src/app/features/shared/components/atoms/MainWrapper/MainWrapper.tsx
@@ -3,27 +3,21 @@ import styled from "styled-components"
 import { breakpoint, themeSpacing } from "@datapunt/asc-ui"
 import { sizes } from "@datapunt/asc-ui/lib/theme/default/breakpoints"
 
-const MAX_WIDTH = sizes.laptopL
-
-const LayoutContainer = styled.div`   
+const Main = styled.main`
   box-sizing: border-box;
   margin: 0 auto;
-  padding: ${ themeSpacing(3) };
+  padding: ${ themeSpacing(6) } ${ themeSpacing(3) };
   width: 100%;
-  max-width: ${ MAX_WIDTH }px;
+  max-width: ${ sizes.laptopL }px;
 
   @media screen and ${ breakpoint("min-width", "laptop") } {
-    padding: ${ themeSpacing(3) } ${ themeSpacing(14) };
+    padding: ${ themeSpacing(6) } ${ themeSpacing(14) };
   }
 `
 const MainWrapper: React.FC = ({ children }) => (
-   
-    <main>
-      <LayoutContainer>
-        { children }
-      </LayoutContainer>
-    </main>
-  
+  <Main>
+    { children }
+  </Main>
 )
 
 export default MainWrapper
