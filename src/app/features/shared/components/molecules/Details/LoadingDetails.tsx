67d8478208be3f2d9ab3244e2c1bfd498d473bf8
Remco Hoff <remco@code010.nl>
2020-10-08 13:54:33 +0200

    Update LoadingDetails.tsx

--- src/app/features/shared/components/molecules/Details/LoadingDetails.tsx
+++ src/app/features/shared/components/molecules/Details/LoadingDetails.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import DefinitionList from "../DefinitionList/DefinitionList"
+import Details from "./Details"
 
 type Props = {
   numRows: number
 }
 const LoadingDetails: React.FC<Props> = ({ numRows }) => (
-  <DefinitionList isLoading={true} title="loading" values={{}} numLoadingRows={numRows} />
+  <Details isLoading={true} title="loading" values={{}} numLoadingRows={numRows} />
 )
 
 export default LoadingDetails
