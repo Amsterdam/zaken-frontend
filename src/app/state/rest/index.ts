859a1f75c676d34d972e80640266dcb30e77cd65
Remco Hoff <remco@code010.nl>
2020-10-27 15:03:04 +0100

    new endpoint permits

--- src/app/state/rest/index.ts
+++ src/app/state/rest/index.ts
@@ -162,7 +162,7 @@ export const usePanorama = (lat?: number, lon?: number, width?: number, aspect?:
 export const usePermitCheckmarks = (bagId: string) => {
   const handleError = useErrorHandler()
   return useApiRequest<{ has_b_and_b_permit: boolean, has_vacation_rental_permit: boolean }>({
-    url: makeGatewayUrl("addresses", bagId, "permits_checkmarks"),
+    url: makeGatewayUrl("addresses", bagId, "permits","checkmarks"),
     groupName: "permits",
     handleError,
     getHeaders
@@ -172,7 +172,7 @@ export const usePermitCheckmarks = (bagId: string) => {
 export const usePermitDetails = (bagId: string) => {
   const handleError = useErrorHandler()
   return useApiRequest<Components.Schemas.DecosPermit[]>({
-    url: makeGatewayUrl("addresses", bagId, "permits_details"),
+    url: makeGatewayUrl("addresses", bagId, "permits"),
     groupName: "permits",
     handleError,
     getHeaders
