ce21de92a874e20dfb6be6c0abd9588433375a14
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-11-06 11:36:23 +0100

    MockWrapper including environment switch

---
+++ src/app/features/shared/components/molecules/MockWrapper/MockWrapper.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import styled from "styled-components"
+import { themeColor } from "@datapunt/asc-ui"
+
+type Props = {
+  children: JSX.Element
+}
+
+type DivProps = {
+  environment: string
+}
+const Div = styled.div<DivProps>`
+  background: ${ ({ environment }) => environment !== "production" ? themeColor("supplement", "pink") : "transparent" };
+  display: ${ ({ environment }) => environment === "production" ? "none" : "block" };
+`
+
+const MockWrapper: React.FC<Props> = ({ children }) => (
+    <Div environment={ process.env.REACT_APP_ENVIRONMENT ?? "development" }>
+      { children }
+    </Div>
+  )
+export default MockWrapper
