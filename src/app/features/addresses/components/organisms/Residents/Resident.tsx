57c6cb82745c1a0efb283fd30ac737abd39166e2
Remco Hoff <remco@code010.nl>
2020-11-16 15:26:18 +0100

    fix typo in Residents (change in back-end)

--- src/app/features/addresses/components/organisms/Residents/Resident.tsx
+++ src/app/features/addresses/components/organisms/Residents/Resident.tsx
@@ -12,8 +12,8 @@ const Resident: React.FC<Props> = ({ resident, num }) => {
   const title = <>{ `${ num }. ` } <PersonDisplay sex={ resident.geslachtsaanduiding } firstName={ `${ resident.voorletters }.` } namePrefix={ resident.voorvoegsel_geslachtsnaam } name={ resident.geslachtsnaam } /></>
   const values = useMemo(() => ({
     "Geboren": <DateDisplay date={ resident.geboortedatum } />,
-    "Ingeschreven sinds": <DateDisplay date={ resident.datum_begin_relatie_verblijadres } />
-  }), [resident.geboortedatum, resident.datum_begin_relatie_verblijadres])
+    "Ingeschreven sinds": <DateDisplay date={ resident.datum_begin_relatie_verblijfadres } />
+  }), [resident.geboortedatum, resident.datum_begin_relatie_verblijfadres])
 
   return <DefinitionList
     numInitialVisibleRows={4}
