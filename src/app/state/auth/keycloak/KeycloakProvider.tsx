a7d0696c3983198f9a73aca06b9bcdfb589458b6
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-11-19 14:42:47 +0100

    Check is-authorized

--- src/app/state/auth/keycloak/KeycloakProvider.tsx
+++ src/app/state/auth/keycloak/KeycloakProvider.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react"
+import { navigate } from "@reach/router"
 
 import { keycloak, Keycloak } from "./keycloak"
+
+import to from "app/features/shared/routing/to"
+import { makeGatewayUrl } from "app/state/rest/hooks/utils/utils"
+import createAuthHeaders from "app/state/rest/hooks/utils/createAuthHeaders"
 
 export type Context = {
   token: string | undefined
@@ -8,22 +13,35 @@ export type Context = {
 }
 export const KeycloakContext = React.createContext<Context|undefined>(undefined)
 
+const options = {
+  onLoad: "login-required",
+  checkLoginIframe: false
+}
+
 const KeycloakProvider: React.FC = ({ children }) => {
   const [token, setToken] = useState<string>()
 
   useEffect(() => {
-    keycloak.init({
-      onLoad: "login-required",
-      checkLoginIframe: false
-    }).then((authenticated: boolean) => {
-      if (authenticated) {
-        setToken(keycloak.token)
-      } else {
-        console.log("Keycloak failed to authenticate")
+    (async () => {
+      try {
+        const isAuthenticated = await keycloak.init(options)
+        if (isAuthenticated) {
+          setToken(keycloak.token)
+          const response = await fetch(makeGatewayUrl("is-authorized"), {
+            headers: {
+              ...createAuthHeaders(keycloak.token),
+              "Content-Type": "application/json"
+            }
+          })
+          const { is_authorized } = await response.json()
+          if (is_authorized === false) navigate(to("/auth"))
+        } else {
+          console.log("Keycloak failed to authenticate")
+        }
+      } catch {
+        console.error("Keycloak failed to initialize")
       }
-    }).catch(() => {
-      console.error("Keycloak failed to initialize")
-    })
+    })()
   }, [])
 
   const value = {
