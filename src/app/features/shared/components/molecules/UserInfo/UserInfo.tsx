c0cec8c5d215c8e2d8caa0c056d3f718219743bc
Remco Hoff <remco@code010.nl>
2020-11-20 11:05:41 +0100

    Refactoring UserInfo

--- src/app/features/shared/components/molecules/UserInfo/UserInfo.tsx
+++ src/app/features/shared/components/molecules/UserInfo/UserInfo.tsx
@@ -1,13 +1,16 @@
+import React from "react"
+import styled from "styled-components"
 import { Logout, PersonalLogin } from "@datapunt/asc-assets"
 import { breakpoint, MenuButton } from "@datapunt/asc-ui"
 
 import useKeycloak from "app/state/auth/keycloak/useKeycloak"
 
-import React from "react"
-import styled from "styled-components"
-
 type Props = {
   showAsListItem?: boolean
+}
+type UserProps = {
+  onClick: () => void
+  userDisplay: string
 }
 
 const UserDisplayStyle = styled.div`
@@ -28,37 +31,44 @@ const UserDisplayStyle = styled.div`
     display: inline-block;
   }
 `
-const linkStyle = `{ 
+const StyledMenuButton = styled(MenuButton)`
   height: 54px; 
   font-weight: normal; 
   padding: 12px 16px 9px;
-}`
+`
+
+const UserDisplay: React.FC<UserProps> = ({ userDisplay, onClick }) => 
+  <>
+    { userDisplay &&
+      <UserDisplayStyle>
+        <PersonalLogin width={24} height={24} />
+        <span>{ userDisplay }</span>
+      </UserDisplayStyle>
+    }
+    <StyledMenuButton
+      tabIndex={0}
+      onClick={ onClick }
+      iconLeft={<Logout/>}
+      title="Uitloggen"
+      iconSize={24}
+    >Uitloggen</StyledMenuButton>
+  </>
 
 const UserInfo: React.FC<Props> = ({ showAsListItem = false }) => {
   const { keycloak } = useKeycloak()
   const onClick = () => keycloak.logout()
   const userDisplay = keycloak.tokenParsed?.name
-  const MenuButtonWrap = showAsListItem 
-    ? styled.li`a ${ linkStyle }`
-    : styled.span`a  ${ linkStyle }`
-
+  
   return (
-    <MenuButtonWrap>
-      { userDisplay &&
-        <UserDisplayStyle>
-          <PersonalLogin width={24} height={24} />
-          <span>{ userDisplay }</span>
-        </UserDisplayStyle>
-      }
-      <MenuButton
-        as="a"
-        tabIndex={0}
-        onClick={ onClick }
-        iconLeft={<Logout/>}
-        title="Uitloggen"
-        iconSize={24}
-      >Uitloggen</MenuButton>
-    </MenuButtonWrap>
+    showAsListItem 
+      ?
+        <li>
+          <UserDisplay userDisplay={userDisplay} onClick={onClick} />
+        </li>
+      :
+        <span>
+          <UserDisplay userDisplay={userDisplay} onClick={onClick} />
+        </span>
   )
 }
 
