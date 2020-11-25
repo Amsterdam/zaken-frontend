a6955801373f2d887b9f7dd145301bb5b958ea73
Remco Hoff <remco@code010.nl>
2020-11-25 17:48:38 +0100

    Refactoring HelpContent

--- src/app/features/debriefings/components/molecules/DebriefForm/scaffold.tsx
+++ src/app/features/debriefings/components/molecules/DebriefForm/scaffold.tsx
@@ -3,40 +3,22 @@ import { FormPositioner } from "amsterdam-scaffold-form/package"
 import { Fields } from "app/features/shared/components/molecules/Form/ScaffoldFields"
 import { navigate } from "@reach/router"
 import InfoButton from "app/features/shared/components/molecules/InfoHeading/InfoButton"
+import { helpTextViolation } from "./HelpContent"
 
 const Scaffold = (caseId: Components.Schemas.Case["id"], isEditing = false) => {
-  const helpTextViolation = <>
-    <ol>
-      <li><b>Nader onderzoek nodig</b>
-        <ul>
-          <li>Iets uitzoeken wat niet op de locatie zelf is. Bijv. advies van de teamleider of jurist.</li>
-          <li>Verwerk de uitkomst vervolgens in een nieuwe debriefnotitie.</li>
-        </ul>
-        <br/><br/>
-      </li>
-      <li><b>Aanvullend huisbezoek nodig</b>
-        <ul>
-          <li>Nader onderzoek nodig op het adres zelf door de toezichthouders.</li>
-          <li>Zet dit bezoek uit via de projectmedewerker.</li>
-          <li>Vermeldt waar specifiek op gelet moet worden.</li>
-          <br/><br/>
-        </ul>
-      </li>
-    </ol>
-  </>
   const fields = {
     violation: {
       type: "RadioFields",
       props: {
         isRequired: true,
-        label: "Is er sprake van een overtreding?",
-        extraLabel: <InfoButton infoTitle="Niet duidelijk of er een overtreding is? Twee opties:" infoText={helpTextViolation}></InfoButton>,
+        label: "Wat is de uitkomst van het huisbezoek?",
+        extraLabel: <InfoButton infoTitle="Niet duidelijk of er een overtreding is? Twee opties:" infoText={ helpTextViolation}></InfoButton>,
         name: "violation",
         options: {
-          "YES": "Ja, overtreding",
-          "NO": "Nee, geen overtreding",
+          "YES": "Overtreding",
+          "NO": "Geen overtreding",
           "ADDITIONAL_RESEARCH_REQUIRED": "Nader onderzoek nodig",
-          "ADDITIONAL_VISIT_REQUIRED": "Nieuw huisbezoek nodig"
+          "ADDITIONAL_VISIT_REQUIRED": "Aanvullend huisbezoek nodig"
         }
       }
     },
