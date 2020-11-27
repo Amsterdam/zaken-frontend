fa38db8ca55f046d3580858488b55a31c9e92892
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-11-27 12:49:41 +0100

    Fixes

--- src/app/features/help/components/molecules/HelpContent/HelpContent.tsx
+++ src/app/features/help/components/molecules/HelpContent/HelpContent.tsx
@@ -18,21 +18,23 @@ const HelpContent: React.FC = () => (
       <Paragraph>
       Werk het zaaksysteem niet (goed)? Probeer de pagina opnieuw te laden door op het refresh icoon te klikken (icoon tonen).<br/>
       Werkt het zaaksysteeem dan nog steeds niet, neem dan contact op met:
+      </Paragraph>
       <ul>
         <li>Tijdens kantooruren ontwikkelteam Zaaksysteem, { EMPLOYEES[0].name } <PhoneLink phoneNumber={ EMPLOYEES[0].phoneNumber } /></li>
         <li>Buiten kantoortijden Datapunt, { EMPLOYEES[1].name } <PhoneLink phoneNumber={ EMPLOYEES[1].phoneNumber } /></li>
       </ul>
-      </Paragraph>
     </RowWithColumn>
     <RowWithColumn>
       <Heading as="h2">Vraag een demo aan</Heading>
       <Paragraph>
       Ben je nieuw met het systeem, loop je ergens tegen aan of wil je gewoon graag persoonlijke uitleg over het zaaksysteem? Neem contact op met { EMPLOYEES[2].name }
+      </Paragraph>
       <ul>
         <li>Telefonisch: te bereiken tijdens kantooruren op ma, di, wo en do op <PhoneLink phoneNumber={ EMPLOYEES[2].phoneNumber } /></li>
         <li>Per e-mail: <EmailLink email={ EMPLOYEES[2].email! } /></li>
       </ul>
-      Ook voor goede ideeën en suggesties <span role="img">🙂</span>
+      <Paragraph>
+      Ook voor goede ideeën en suggesties <span role="img" aria-label="Slightly Smiling Face">🙂</span>
       </Paragraph>
     </RowWithColumn>
   </>
