5956201602ad19ac70cd97171360f988c3ea8abc
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-11-27 12:34:23 +0100

    Use Paragraph tags

--- src/app/features/help/components/molecules/HelpContent/HelpContent.tsx
+++ src/app/features/help/components/molecules/HelpContent/HelpContent.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Heading } from "@datapunt/asc-ui"
+import { Heading, Paragraph } from "@datapunt/asc-ui"
 
 import { RowWithColumn } from "app/features/shared/components/atoms/Grid/"
 import EmailLink from "./EmailLink"
@@ -15,25 +15,25 @@ const HelpContent: React.FC = () => (
   <>
     <RowWithColumn>
       <Heading as="h2">Technisch support</Heading>
-      <p>
+      <Paragraph>
       Werk het zaaksysteem niet (goed)? Probeer de pagina opnieuw te laden door op het refresh icoon te klikken (icoon tonen).<br/>
       Werkt het zaaksysteeem dan nog steeds niet, neem dan contact op met:
-      </p>
       <ul>
         <li>Tijdens kantooruren ontwikkelteam Zaaksysteem, { EMPLOYEES[0].name } <PhoneLink phoneNumber={ EMPLOYEES[0].phoneNumber } /></li>
         <li>Buiten kantoortijden Datapunt, { EMPLOYEES[1].name } <PhoneLink phoneNumber={ EMPLOYEES[1].phoneNumber } /></li>
       </ul>
+      </Paragraph>
     </RowWithColumn>
     <RowWithColumn>
       <Heading as="h2">Vraag een demo aan</Heading>
-      <p>
+      <Paragraph>
       Ben je nieuw met het systeem, loop je ergens tegen aan of wil je gewoon graag persoonlijke uitleg over het zaaksysteem? Neem contact op met { EMPLOYEES[2].name }
-      </p>
       <ul>
         <li>Telefonisch: te bereiken tijdens kantooruren op ma, di, wo en do op <PhoneLink phoneNumber={ EMPLOYEES[2].phoneNumber } /></li>
         <li>Per e-mail: <EmailLink email={ EMPLOYEES[2].email! } /></li>
       </ul>
-      <p>Ook voor goede ideeën en suggesties <span role="img">🙂</span></p>
+      Ook voor goede ideeën en suggesties <span role="img">🙂</span>
+      </Paragraph>
     </RowWithColumn>
   </>
 )
