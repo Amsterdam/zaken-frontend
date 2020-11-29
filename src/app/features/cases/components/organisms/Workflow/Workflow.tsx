31afa85b8c6a41045a9c91531f0a856d82a0ab1f
Remco Hoff <remco@code010.nl>
2020-11-29 14:52:16 +0100

    Added table for Debrief

--- src/app/features/cases/components/organisms/Workflow/Workflow.tsx
+++ src/app/features/cases/components/organisms/Workflow/Workflow.tsx
@@ -1,13 +1,46 @@
 import React from "react"
-import { Divider, Heading } from "@datapunt/asc-ui"
+import { Button, Divider, Heading, Icon, themeColor } from "@datapunt/asc-ui"
+import styled from "styled-components"
 
-import CreateDebriefingLink from "app/features/cases/components/organisms/CreateDebriefingLink/CreateDebriefingLink"
 import { useCaseEvents } from "app/state/rest"
 import workflow from "app/state/workflow/workflow"
+import Table from "app/features/shared/components/molecules/Table/Table"
+import { EditDocument } from "@datapunt/asc-assets"
+import ButtonLink from "app/features/shared/components/atoms/ButtonLink/ButtonLink"
+import to from "app/features/shared/routing/to"
 
 type Props = {
   caseId: Components.Schemas.Case["id"]
 }
+
+const StyledTable = styled(Table)`
+  color: red;
+  line-height: 48px;
+  thead {
+    color: ${ themeColor("tint", "level4") }
+  }
+`
+
+const columns = [
+  { minWidth: 50 },
+  { header: "Actuele taken", minWidth: 100 },
+  { header: "Uitvoerder", minWidth: 100 },
+  { header: "Einddatum", minWidth: 100 },
+  { header: "Looptijd", minWidth: 100 },
+  { header: "Actie", minWidth: 140 }
+]
+
+const workflowDebrief = (caseId: number) => [[
+  <Icon size={ 20 }><EditDocument /></Icon>,
+  "Verwerken Debrief",
+  "ProjectHandhaver",
+  "-",
+  "-",
+  <ButtonLink to={ to("/cases/:caseId/debriefing", { caseId })}>
+    <Button variant="primary" as="span">Debrief verwerken</Button>
+  </ButtonLink>
+]]
+
 
 const Workflow: React.FC<Props> = ({ caseId }) => {
   const { data } = useCaseEvents(caseId)
@@ -29,7 +62,11 @@ const Workflow: React.FC<Props> = ({ caseId }) => {
         </ul>
       }
       { shouldCreateDebriefing &&
-        <CreateDebriefingLink id={ caseId } />
+        <StyledTable
+          columns={columns}
+          data={ workflowDebrief(caseId) }
+          noValuesPlaceholder=""
+        />
       }
       { shouldCloseCase &&
         <>
