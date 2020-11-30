9506fc96042bff00995a77f6aec6efe2f110acb0
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-11-30 15:14:27 +0100

    Simplify CSS layout

--- src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/app/features/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { breakpoint, Header } from "@datapunt/asc-ui"
+import { Header } from "@datapunt/asc-ui"
 import styled from "styled-components"
 import DefaultNavigation from "app/features/shared/components/molecules/navigation/DefaultNavigation"
 import MainWrapper from "app/features/shared/components/atoms/MainWrapper/MainWrapper"
@@ -11,34 +11,26 @@ type Props = {
   showSearchButton?: boolean
 }
 
-const Div = styled.div`
-  position: relative;
-  z-index: 2;
-`
-const MenuWithSearchButtonWrap = styled.div`
+const MenuWrap = styled.div`
   display: flex;
   justify-content: space-between;
-
-  @media screen and ${ breakpoint("min-width", "laptopM") } {
-    margin-right: auto;
-    width: 100%;
-  }
+  width: 100%;
 `
 
 const DefaultLayout: React.FC<Props> = ({ showSearchButton = true, children }) => (
   <>
-    <Div>
-      <Header
-        tall
-        fullWidth={false}
-        title="Zaaksysteem Wonen"
-        homeLink={to("/")}
-        navigation={<MenuWithSearchButtonWrap>
-          <DefaultNavigation showSearchButton={showSearchButton} />
-        </MenuWithSearchButtonWrap>}
-        links={<UserInfo />}
-      />
-    </Div>
+    <Header
+      tall
+      fullWidth={ false }
+      title="Zaaksysteem Wonen"
+      homeLink={ to("/") }
+      navigation={
+        <MenuWrap>
+          <DefaultNavigation showSearchButton={ showSearchButton } />
+        </MenuWrap>
+      }
+      links={ <UserInfo /> }
+    />
     <MainWrapper>
       <FlashMessages />
       { children }
