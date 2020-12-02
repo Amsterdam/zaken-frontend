79fbcb49d9edad605f2400193d00f093df42d4ef
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-12-02 16:42:14 +0100

    Use npm keycloak-js within React bundle

--- src/app/state/auth/keycloak/keycloak.ts
+++ src/app/state/auth/keycloak/keycloak.ts
@@ -1,11 +1,16 @@
 import settings from "./settings"
-import keycloakMock from "./keycloak.mock"
-import isLocalDevelopment from "./isLocalDevelopment"
+//import keycloakMock from "./keycloak.mock"
+//import isLocalDevelopment from "./isLocalDevelopment"
+import Keycloak from "keycloak-js"
 
+/*
 const keycloakAvailable = (window as any).Keycloak !== undefined
 export const keycloak = keycloakAvailable && isLocalDevelopment === false ?
   new (window as any).Keycloak(settings) :
   keycloakMock
+export type Keycloak = typeof keycloak
+*/
+export const keycloak = new (Keycloak as any)(settings)
 export type Keycloak = typeof keycloak
 
 if (process.env.NODE_ENV === "development") {
