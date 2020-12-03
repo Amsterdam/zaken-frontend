ff555395850f10694ea5a4b0f9145597c49d6e82
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-12-03 13:37:54 +0100

    Use npm keycloak-js

--- src/app/state/auth/keycloak/settings.ts
+++ src/app/state/auth/keycloak/settings.ts
@@ -1,6 +1,6 @@
 export default {
+  "url": "https://iam.amsterdam.nl/auth/",
   "realm": process.env.REACT_APP_KEYCLOAK_REALM ?? "datapunt-ad-acc",
-  "auth-server-url": "https://iam.amsterdam.nl/auth/",
   "ssl-required": "external",
   "resource": "wonen-zaaksysteem-frontend",
   "public-client": true,
