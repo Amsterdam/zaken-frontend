2c26d0d085fae8def4a5ea35cf843abaf018ed58
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-12-17 16:44:32 +0100

    Use reduce for allEventsInTime

--- src/app/features/cases/components/CaseTimeline/hooks/useGroupedCaseEvents.ts
+++ src/app/features/cases/components/CaseTimeline/hooks/useGroupedCaseEvents.ts
@@ -9,31 +9,15 @@ type EventProps = {
 export default (caseId: Components.Schemas.CaseEvent["id"]) => {
 
   const { data } = useCaseEvents(caseId)
+  
+  const allEventsInTime = data?.reduce((acc, item, index) => {
+    if (acc[index - 1]?.type !== item.type) {
+      acc.push({ index, type: item.type, eventList: [item] })
+    } else {
+      acc[index - 1].eventList.push(item)
+    }
+    return acc
+  }, [] as EventProps[])
 
-  let currentIndex = -1
-  let currentType = ""
-  let previousType = ""
-  const allEventsInTime: EventProps[] = []
-
-  const startNewEventList = (event: Components.Schemas.CaseEvent) => {
-    previousType = currentType
-    currentIndex++
-    allEventsInTime.push({ index: currentIndex, type: currentType, eventList: [event] })
-  }
-
-  const addEventToList = (event: Components.Schemas.CaseEvent) => {
-    allEventsInTime[currentIndex].eventList?.push(event)
-  }
-
-  const doGroupEvents = (item: Components.Schemas.CaseEvent) => {
-    currentType = item.type
-    currentType !== previousType ?
-      startNewEventList(item) :
-      addEventToList(item)
-  }
-
-  // TODO order list by date
-  data?.forEach(doGroupEvents)
-
-  return allEventsInTime
+  return allEventsInTime ?? []
 }
