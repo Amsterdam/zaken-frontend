d1ccbf0aa11fd918f0de26b5c0a66521bd38da7f
Remco Hoff <remco@code010.nl>
2021-01-06 11:42:50 +0100

    fix naming environment

--- src/app/sentry/init.ts
+++ src/app/sentry/init.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/browser"
 
 export default () => {
   const dsn = process.env.REACT_APP_SENTRY_DSN
-  const environment = process.env.REACT_APP_SENTRY_ENVIRONMENT
+  const environment = process.env.REACT_APP_ENVIRONMENT
   const release = process.env.REACT_APP_GIT_COMMIT_HASH
   if (dsn === undefined || environment === undefined) return
   
