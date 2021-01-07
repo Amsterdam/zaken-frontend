a07e853b910d1d30e945b5b5b1235cbcd242f4b3
Remco Hoff <remco@code010.nl>
2021-01-07 15:52:25 +0100

    Added CaseDetails

---
+++ src/app/features/cases/components/CaseDetails/CaseDetails.tsx
@@ -0,0 +1,24 @@
+import React, { useMemo } from "react"
+import DefinitionList from "app/features/shared/components/molecules/DefinitionList/DefinitionList"
+import { useCase } from "app/state/rest"
+import { displayDate } from "app/features/shared/components/atoms/DateDisplay/DateDisplay"
+
+type Props = {
+  caseId: Components.Schemas.CaseEvent["id"]
+}
+
+const CaseDetails: React.FC<Props> = ({ caseId }) => {
+  const { data } = useCase(caseId)
+  const values = useMemo(() => ({
+    // TODO use real data
+    "Zaak-ID": data?.id,
+    "Team": "Vakantieverhuur",
+    "Startdatum": data?.start_date ? `${ displayDate(data?.start_date) }` : "-"
+  }), [ data ])
+  return <DefinitionList
+    numInitialVisibleRows={3}
+    values={values}
+  />
+}
+
+export default CaseDetails
