ef3b851c6ab67a20005cdb56d570dfae9335209d
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-01-20 11:09:56 +0100

    Use WorkflowForm for CompleteCaseForm

--- src/app/pages/cases/complete/CompleteCasePage.tsx
+++ src/app/pages/cases/complete/CompleteCasePage.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import { RouteComponentProps } from "@reach/router"
 import { FormTitle } from "@amsterdam/asc-ui"
 
+import { useCompleteCase } from "app/state/rest/"
 import { RowWithColumn } from "app/features/shared/components/atoms/Grid/Row"
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
 import Breadcrumbs from "app/features/shared/components/molecules/BreadCrumbs/BreadCrumbs"
 import PageHeading from "app/features/shared/components/molecules/PageHeading/PageHeading"
-import CompleteCaseForm from "app/features/cases/components/CompleteForm/CompleteCaseForm"
+import WorkflowForm from "app/features/cases/components/Workflow/WorkflowForm"
 import AddressHeading from "app/features/shared/components/molecules/AddressHeading/AddressHeading"
+import scaffold from "app/features/cases/components/CompleteForm/scaffold"
 
 type Props = {
   id: string
@@ -16,6 +18,8 @@ type Props = {
 const CompleteCasePage: React.FC<RouteComponentProps<Props>> = ({ id: idString }) => {
 
   const id: Components.Schemas.Case["id"] = parseInt(idString!)
+  const endpoint = useCompleteCase()
+  const { execPost } = endpoint
 
   return (
     <DefaultLayout>
@@ -28,7 +32,12 @@ const CompleteCasePage: React.FC<RouteComponentProps<Props>> = ({ id: idString }
       <RowWithColumn>
         <FormTitle>Gebruik dit formulier om de zaak af te ronden</FormTitle>
         <AddressHeading caseId={ id } />
-        <CompleteCaseForm caseId={ id! } />
+        <WorkflowForm
+          caseId={ id }
+          endpoint={ endpoint }
+          postMethod = { execPost }
+          scaffold= { scaffold }
+        />
       </RowWithColumn>
     </DefaultLayout>
   )
