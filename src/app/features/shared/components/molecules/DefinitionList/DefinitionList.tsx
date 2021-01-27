e8174c6dcb6a28ead2ade893525b3d7c1c9fd7d4
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-01-27 11:10:58 +0100

    Improve styling DefinitionList

--- src/app/features/shared/components/molecules/DefinitionList/DefinitionList.tsx
+++ src/app/features/shared/components/molecules/DefinitionList/DefinitionList.tsx
@@ -17,26 +17,28 @@ type Props = {
   headingSize?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
 }
 
-const StyledDL = styled.dl`
+const Div = styled.div`
+  overflow: auto;
+  border-bottom: 1px solid ${ themeColor("tint","level3") };
+  &:last-child {
+    border-bottom: none;
+  }
+`
+
+const Dl = styled.dl`
   width: 100%;
   margin-bottom: ${ themeSpacing(10) };
 
-  &:after {
-    clear: both;
-    content: "";
-    display: table;
-  }
-
   dd, dt {
     width: 50%;
-    border-bottom: 1px solid ${ themeColor("tint","level3") };
-    padding: ${ themeSpacing(3) } 0; 
+    padding: ${ themeSpacing(3) } 0;
   }
   dt {
     float: left;
     clear: both;
+    padding-right: ${ themeSpacing(5) };
     word-wrap: break-word;
-    padding-right: ${ themeSpacing(5) }; 
+    color: ${ themeColor("tint", "level5") };
   }
   dd {
     margin: 0;
@@ -79,25 +81,30 @@ const DefinitionList: React.FC<Props> = ({ isLoading, numLoadingRows, numInitial
 
   return (
     <div>
-      { title && <Heading forwardedAs={ headingSize }>
-        { isLoading ? <SmallSkeleton height={10} /> : title}
-        { extraInfo && <InfoButton infoTitle={extraInfo.infoTitle} infoText={extraInfo.infoText} /> }
-        </Heading> }
-      <StyledDL>
-        { isLoading
-          ? <LoadingRows numRows={numLoadingRows ?? 5} />
-          : <>
-              { rows
-                .map(([key, value]) => (
-                  <div key={key}>
-                    <dt>{ key }</dt>
-                    <dd>{ castValue(value) || "-" }</dd>
-                  </div>
-                )) }
-            </>
+      { title &&
+        <Heading forwardedAs={ headingSize }>
+          { isLoading ? <SmallSkeleton height={10} /> : title }
+          { extraInfo &&
+            <InfoButton infoTitle={ extraInfo.infoTitle } infoText={ extraInfo.infoText } />
           }
-      </StyledDL>
-      { isCollapsible && <ToggleCollapse onClick={toggleCollapsed} isCollapsed={isCollapsed} /> }
+        </Heading>
+      }
+      <Dl>
+        { isLoading ?
+          <LoadingRows numRows={numLoadingRows ?? 5} /> :
+          <>
+            { rows.map(([key, value]) => (
+              <Div key={key}>
+                <dt>{ key }</dt>
+                <dd>{ castValue(value) }</dd>
+              </Div>
+            )) }
+          </>
+        }
+      </Dl>
+      { isCollapsible &&
+        <ToggleCollapse onClick={toggleCollapsed} isCollapsed={isCollapsed} />
+      }
     </div>
   )
 }
