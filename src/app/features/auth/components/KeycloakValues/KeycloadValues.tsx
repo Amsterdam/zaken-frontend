75ef5017c41e567e4a5a17edd32c904ed5bb2159
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-02-02 11:43:37 +0100

    AuthPage

---
+++ src/app/features/auth/components/KeycloakValues/KeycloadValues.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+import type KeycloakTokenParsedExtended from "app/state/auth/keycloak/KeycloakTokenParsedExtended"
+import useKeycloak from "app/state/auth/keycloak/useKeycloak"
+import DefinitionList from "app/features/shared/components/molecules/DefinitionList/DefinitionList"
+
+const KeycloakValues: React.FC = () => {
+  const keycloak = useKeycloak()
+  const tokenParsed = keycloak.tokenParsed as KeycloakTokenParsedExtended
+
+  const values = {
+    Naam: tokenParsed?.name ?? "-",
+    "E-mail": tokenParsed?.email ?? "-",
+    Gebruikersnaam: tokenParsed?.preferred_username ?? "-",
+    "Keycloak groepen": keycloak.realmAccess?.roles.join(", ") ?? "-"
+  }
+
+  return (
+    <DefinitionList values={ values } />
+  )
+}
+
+export default KeycloakValues
---
+++ src/app/features/auth/components/NotAuthorizedAlert/NotAuthorizedAlert.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Alert } from "@amsterdam/asc-ui"
+
+import { useIsAuthorized } from "app/state/rest/"
+
+const NotAuthorizedAlert = () => {
+
+  const { data } = useIsAuthorized()
+  const showUnauthorized = data?.isAuthorized === false
+
+  return showUnauthorized ?
+    <Alert level="error">Je bent niet geauthorizeerd. Waarschijnlijk staan de Keycloak groepen gekoppeld aan je ADW account niet goed ingesteld.</Alert> :
+    null
+}
+export default NotAuthorizedAlert
