cd6386383940abcfafe0014e37fe435df0758bd4
Remco Hoff <remco@code010.nl>
2021-02-04 17:57:22 +0100

    Refactoring Create Summon

--- src/app/pages/case/summons/CreatePage.tsx
+++ src/app/pages/case/summons/CreatePage.tsx
@@ -1,28 +1,24 @@
 import React from "react"
 import { RouteComponentProps } from "@reach/router"
-import { FormTitle } from "@amsterdam/asc-ui"
 
-import { useSummons } from "app/state/rest/"
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
 import PageHeading from "app/features/shared/components/molecules/PageHeading/PageHeading"
 import BreadCrumbs from "app/features/shared/components/molecules/BreadCrumbs/BreadCrumbs"
 import Row, { RowWithColumn } from "app/features/shared/components/atoms/Grid/Row"
-import scaffold from "app/features/summons/components/SummonForm/scaffold"
-import FormWithExtraLabel from "app/features/shared/components/atoms/FormWithExtraLabel/FormWithExtraLabel"
 import parseUrlParamId from "app/routing/utils/parseUrlParamId"
 import isValidUrlParamId from "app/routing/utils/isValidUrlParamId"
 import NotFoundPage from "app/features/shared/components/pages/NotFoundPage"
 import CaseHeading from "app/features/cases/components/CaseHeading/CaseHeading"
-import WorkflowForm from "app/features/cases/components/Workflow/WorkflowForm"
 import { Column } from "app/features/shared/components/atoms/Grid"
+import SummonForm from "app/features/summons/components/SummonForm/SummonForm"
 
 type Props = {
   id: string
 }
 
 const CreatePage: React.FC<RouteComponentProps<Props>> = ({ id: idString }) => {
+
   const id = parseUrlParamId(idString)
-  const { data, execPost } = useSummons()
 
   return (
     isValidUrlParamId<Components.Schemas.Case["id"]>(id) ?
@@ -38,15 +34,7 @@ const CreatePage: React.FC<RouteComponentProps<Props>> = ({ id: idString }) => {
       </RowWithColumn>
       <Row>
         <Column spanLarge={50}>
-          <FormTitle>Gebruik dit formulier om aan te geven welke aanschrijving(en) opgesteld is en voor wie. Vul dit in nadat brief verstuurd is!</FormTitle>
-          <FormWithExtraLabel>
-            <WorkflowForm
-              caseId={ id! }
-              data={ data }
-              postMethod={ execPost }
-              scaffold= { scaffold }
-            />
-          </FormWithExtraLabel>
+          <SummonForm id={ id } />
           </Column>
         </Row>
     </DefaultLayout> :
