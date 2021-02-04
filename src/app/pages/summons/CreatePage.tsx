7f297fcf2b5dffc767f78baa3b4acfd4ca65f38c
Remco Hoff <remco@code010.nl>
2021-02-04 13:44:04 +0100

    Use generic WorkflowForm

--- src/app/pages/summons/CreatePage.tsx
+++ src/app/pages/summons/CreatePage.tsx
@@ -7,13 +7,13 @@ import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/
 import PageHeading from "app/features/shared/components/molecules/PageHeading/PageHeading"
 import BreadCrumbs from "app/features/shared/components/molecules/BreadCrumbs/BreadCrumbs"
 import { RowWithColumn } from "app/features/shared/components/atoms/Grid/Row"
-import SummonForm from "app/features/summons/components/SummonForm/SummonForm"
 import scaffold from "app/features/summons/components/SummonForm/scaffold"
 import FormWithExtraLabel from "app/features/shared/components/atoms/FormWithExtraLabel/FormWithExtraLabel"
 import parseUrlParamId from "app/routing/utils/parseUrlParamId"
 import isValidUrlParamId from "app/routing/utils/isValidUrlParamId"
 import NotFoundPage from "app/features/shared/components/pages/NotFoundPage"
 import CaseHeading from "app/features/cases/components/CaseHeading/CaseHeading"
+import WorkflowForm from "app/features/cases/components/Workflow/WorkflowForm"
 
 type Props = {
   id: string
@@ -36,7 +36,7 @@ const CreatePage: React.FC<RouteComponentProps<Props>> = ({ id: idString }) => {
         <CaseHeading id={ id } />
         <FormTitle>Gebruik dit formulier om aan te geven welke aanschrijving(en) opgesteld is en voor wie. Vul dit in nadat brief verstuurd is!</FormTitle>
         <FormWithExtraLabel>
-          <SummonForm
+          <WorkflowForm
             caseId={ id! }
             data={ data }
             postMethod={ execPost }
