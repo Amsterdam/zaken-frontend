8eec5036c82fbf45931eb52fb70b75eabc9247e3
Remco Hoff <remco@code010.nl>
2021-02-04 19:06:01 +0100

    Refactoring Create Correspondence

---
+++ src/app/features/correspondence/components/CorrespondenceForm/CorrespondenceForm.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { FormTitle } from "@amsterdam/asc-ui"
+
+import { useCorrespondence } from "app/state/rest/"
+import WorkflowForm from "app/features/cases/components/Workflow/WorkflowForm"
+import scaffold from "app/features/correspondence/components/CorrespondenceForm/scaffold"
+import FormWithExtraLabel from "app/features/shared/components/atoms/FormWithExtraLabel/FormWithExtraLabel"
+
+type Props = {
+  id: Components.Schemas.Case["id"]
+}
+
+const CorrespondenceForm: React.FC<Props> = ({ id }) => {
+
+    const { data, execPost } = useCorrespondence()
+
+  return (
+    <>
+      <FormTitle>Gebruik dit formulier om notitie van correspondentie toe te voegen</FormTitle>
+      <FormWithExtraLabel>
+        <WorkflowForm
+          caseId={ id! }
+          data={ data }
+          postMethod={ execPost }
+          scaffold= { scaffold }
+        />
+        </FormWithExtraLabel>
+    </>
+  )
+}
+
+export default CorrespondenceForm
--- src/app/pages/case/correspondence/CreatePage.tsx
+++ src/app/pages/case/correspondence/CreatePage.tsx
@@ -1,28 +1,24 @@
 import React from "react"
 import { RouteComponentProps } from "@reach/router"
-import { FormTitle } from "@amsterdam/asc-ui"
 
 import DefaultLayout from "app/features/shared/components/layouts/DefaultLayout/DefaultLayout"
 import PageHeading from "app/features/shared/components/molecules/PageHeading/PageHeading"
 import BreadCrumbs from "app/features/shared/components/molecules/BreadCrumbs/BreadCrumbs"
 import Row, { RowWithColumn } from "app/features/shared/components/atoms/Grid/Row"
-import WorkflowForm from "app/features/cases/components/Workflow/WorkflowForm"
-import { useCorrespondence } from "app/state/rest"
-import scaffold from "app/features/correspondence/components/CorrespondenceForm/scaffold"
-import FormWithExtraLabel from "app/features/shared/components/atoms/FormWithExtraLabel/FormWithExtraLabel"
 import parseUrlParamId from "app/routing/utils/parseUrlParamId"
 import isValidUrlParamId from "app/routing/utils/isValidUrlParamId"
 import NotFoundPage from "app/features/shared/components/pages/NotFoundPage"
 import CaseHeading from "app/features/cases/components/CaseHeading/CaseHeading"
 import { Column } from "app/features/shared/components/atoms/Grid"
+import CorrespondenceForm from "app/features/correspondence/components/CorrespondenceForm/CorrespondenceForm"
 
 type Props = {
   id: string
 }
 
 const CreatePage: React.FC<RouteComponentProps<Props>> = ({ id: idString }) => {
+  
   const id = parseUrlParamId(idString)
-  const { data, execPost } = useCorrespondence()
 
   return (
     isValidUrlParamId<Components.Schemas.Case["id"]>(id) ?
@@ -38,15 +34,7 @@ const CreatePage: React.FC<RouteComponentProps<Props>> = ({ id: idString }) => {
       </RowWithColumn>
       <Row>
         <Column spanLarge={50}>
-          <FormTitle>Gebruik dit formulier om notitie van correspondentie toe te voegen</FormTitle>
-          <FormWithExtraLabel>
-            <WorkflowForm
-              caseId={ id! }
-              data={ data }
-              postMethod = { execPost }
-              scaffold= { scaffold }
-            />
-          </FormWithExtraLabel>
+          <CorrespondenceForm id={ id } />
         </Column>
       </Row>
     </DefaultLayout> :
