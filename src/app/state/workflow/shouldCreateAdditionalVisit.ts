9a124eee35a3cb84faf1bd30e0d507bbe49bcbad
Remco Hoff <remco@code010.nl>
2021-02-09 11:20:20 +0100

    Removed unused workflow files

--- src/app/state/workflow/shouldCreateAdditionalVisit.ts
+++
@@ -1,6 +0,0 @@
-export default (caseEvents: Components.Schemas.CaseEvent[], checkShowAsNextStep?: boolean) => 
-  caseEvents[0].type === "DEBRIEFING" &&
-  caseEvents
-    .filter(({ type }) => type === "DEBRIEFING").length > 0 &&
-  caseEvents
-    .filter(({ type }) => type === "DEBRIEFING")[0].event_values.violation === "ADDITIONAL_VISIT_REQUIRED"
--- src/app/state/workflow/shouldCreateViolation.ts
+++
@@ -1,6 +0,0 @@
-export default (caseEvents: Components.Schemas.CaseEvent[]) =>
-  
-  caseEvents
-      .filter(({ type }) => type === "DEBRIEFING")
-      .filter(({ event_values }) => event_values.violation === "YES").length > 0
-      
