09857a35fa0f2ab0b422cb73cb1a1318b9ff63bb
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-02-11 14:56:02 +0100

    Data showDate

--- src/app/components/cases/CaseTimeline/EventData.tsx
+++ src/app/components/cases/CaseTimeline/EventData.tsx
@@ -1,14 +1,22 @@
 import React from "react"
 import { Dl } from "./helpers/Helpers"
+import { displayDate } from "app/components/shared/DateDisplay/DateDisplay"
 
 type Props = {
-  values: Record<string, React.ReactNode>
+  values: Record<string, any>
   fields: string[]
   labelsMap: Record<string, string>
+  showDate?: boolean
 }
 
-const EventData: React.FC<Props> = ({ values, fields, labelsMap }) => (
+const EventData: React.FC<Props> = ({ values, fields, labelsMap, showDate = false }) => (
   <Dl>
+    { showDate && values.date_added &&
+      <div>
+        <dt>Datum</dt>
+        <dd>{ displayDate(values.date_added) }</dd>
+      </div>
+    }
     { fields.map(field => (
       <div key={ field }>
         <dt>{ labelsMap[field] ?? "-" }</dt>
