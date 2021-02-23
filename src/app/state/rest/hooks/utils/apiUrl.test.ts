47d805f69961182fdd81a005c9beff33d89d8fd4
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-02-23 11:19:06 +0100

    Add test for stripApiHostFromUrl

--- src/app/state/rest/hooks/utils/apiUrl.test.ts
+++ src/app/state/rest/hooks/utils/apiUrl.test.ts
@@ -1,7 +1,13 @@
-import { makeApiUrl } from "./apiUrl"
+import { makeApiUrl, stripApiHostFromUrl } from "./apiUrl"
 
-describe("makeGatewayURL", () => {
-  it("should make a GATEWAY url", () => {
+describe("makeApiUrl", () => {
+  it("should make an Api url", () => {
     expect(makeApiUrl("foo", "bar")).toEqual("http://localhost:8080/api/v1/foo/bar/")
+  })
+})
+
+describe("stripApiHostFromUrl", () => {
+  it("should make a GATEWAY url", () => {
+    expect(stripApiHostFromUrl("http://localhost:8080/api/v1/foo/bar/")).toEqual("foo/bar/")
   })
 })
