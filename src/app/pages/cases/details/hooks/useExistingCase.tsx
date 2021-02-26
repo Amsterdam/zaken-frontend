3d43e0eb2727f42f243dda3a3ecf5c1fd0db4a39
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-02-26 12:43:52 +0100

    Commit new files

---
+++ src/app/pages/cases/details/hooks/useExistingCase.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+
+import isValidUrlParamId from "app/routing/utils/isValidUrlParamId"
+import { useCase } from "app/state/rest"
+
+export default (id: number | undefined) => {
+
+  const valid = isValidUrlParamId<Components.Schemas.Case["id"]>(id)
+
+  const [caseItem, { execGet, isBusy }] = useCase(id, { lazy: true })
+
+  useEffect(() => {
+    if (valid === false) return
+    execGet()
+  }, [])
+
+  if (valid === false) return false
+  if (isBusy) return undefined
+  return caseItem !== undefined
+}
