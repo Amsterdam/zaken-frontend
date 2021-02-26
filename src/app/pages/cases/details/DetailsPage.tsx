635a64d8c508acb862d5f68b418d0422d72a91d2
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-02-26 12:33:30 +0100

    Check if case exists

--- src/app/pages/cases/details/DetailsPage.tsx
+++ src/app/pages/cases/details/DetailsPage.tsx
@@ -8,11 +8,12 @@ import PageHeading from "app/components/shared/PageHeading/PageHeading"
 import TimelineContainer from "app/components/cases/CaseTimeline/TimelineContainer"
 import CaseDetails from "app/components/cases/CaseDetails/CaseDetails"
 import parseUrlParamId from "app/routing/utils/parseUrlParamId"
-import isValidUrlParamId from "app/routing/utils/isValidUrlParamId"
 import NotFoundPage from "app/pages/errors/NotFoundPage"
 import DetailHeaderByCaseId from "app/components/shared/DetailHeader/DetailHeaderByCaseId"
 import { Column } from "app/components/layouts/Grid"
 import CaseStatus from "app/components/cases/CaseStatus/CaseStatus"
+import useExistingCase from "./hooks/useExistingCase"
+import PageSpinner from "app/components/shared/PageSpinner/PageSpinner"
 
 type Props = {
   id: string
@@ -21,31 +22,39 @@ type Props = {
 const DetailsPage: React.FC<RouteComponentProps<Props>> = ({ id: idString }) => {
 
   const id = parseUrlParamId(idString)
+  const caseExists = useExistingCase(id)
+  const showSpinner = caseExists === undefined
+  const showCase = caseExists === true
+  const showNotFound = caseExists === false
 
   return (
-    isValidUrlParamId<Components.Schemas.Case["id"]>(id) ?
-    <DefaultLayout>
-      <DetailHeaderByCaseId caseId={ id } enableSwitch={ false } />
-      <RowWithColumn>
-        <PageHeading />
-      </RowWithColumn>
-      <Row>
-        <Column spanLarge={ 78 }>
-          <CaseDetails caseId={ id } />
-        </Column>
-      </Row>
-      <RowWithColumn>
-        <CaseStatus id={ id } />
-      </RowWithColumn>
-      <RowWithColumn>
-        <Heading as="h2">Zaakhistorie</Heading>
-        <Divider />
-      </RowWithColumn>
-      <RowWithColumn>
-        <TimelineContainer caseId={ id } />
-      </RowWithColumn>
-    </DefaultLayout> :
-    <NotFoundPage />
+    showSpinner ?
+      <PageSpinner /> :
+    showCase ?
+      <DefaultLayout>
+        <DetailHeaderByCaseId caseId={ id! } enableSwitch={ false } />
+        <RowWithColumn>
+          <PageHeading />
+        </RowWithColumn>
+        <Row>
+          <Column spanLarge={ 78 }>
+            <CaseDetails caseId={ id! } />
+          </Column>
+        </Row>
+        <RowWithColumn>
+          <CaseStatus id={ id! } />
+        </RowWithColumn>
+        <RowWithColumn>
+          <Heading as="h2">Zaakhistorie</Heading>
+          <Divider />
+        </RowWithColumn>
+        <RowWithColumn>
+          <TimelineContainer caseId={ id! } />
+        </RowWithColumn>
+      </DefaultLayout> :
+    showNotFound ?
+      <NotFoundPage /> :
+      null
   )
 }
 
