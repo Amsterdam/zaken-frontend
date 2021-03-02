a597596b7c2488b85681f9867287816b88c412c5
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-03-02 17:10:39 +0100

    Small fixes

--- src/app/components/case/tasks/CamundaTask/utils/mapCamundaToInitialValues.ts
+++ src/app/components/case/tasks/CamundaTask/utils/mapCamundaToInitialValues.ts
@@ -1,12 +1,12 @@
 export default (camundaForm: Components.Schemas.CamundaTask["form"]) =>
   camundaForm.reduce((acc: Record<string, string>, item: Components.Schemas.CamundaTask["form"]["name"]) => {
-    const { default_value, name, label, type } = item
+    const { default_value, name, type } = item
     if (type === "checkbox" && default_value === "true") {
-      acc[name] = label
+      acc[name] = name
       return acc
     }
     if (type === "select") {
-      acc[name] = "0"
+      acc[name] = default_value
       return acc
     }
     if (default_value == null) return acc
