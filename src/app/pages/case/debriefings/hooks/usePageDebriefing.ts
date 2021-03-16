85570e3e82c4f58f3eb86511a90ef200991556d3
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-03-16 15:07:58 +0100

    Remove code, files

--- src/app/pages/case/debriefings/hooks/usePageDebriefing.ts
+++ src/app/pages/case/debriefings/hooks/usePageDebriefing.ts
@@ -5,7 +5,7 @@ import { useFlashMessages } from "app/state/flashMessages/useFlashMessages"
 import navigateTo from "app/routing/navigateTo"
 
 const usePageDebriefing = (caseId?: Components.Schemas.Case["id"], id?: Components.Schemas.Debriefing["id"]) => {
-  const [, { execPost, execPut, execDelete }] = useDebriefings(id, { lazy: true })
+  const [, { execPost }] = useDebriefings(id, { lazy: true })
   const { addSuccessFlashMessage } = useFlashMessages()
 
   const path = `/zaken/${ caseId ?? "" }`
@@ -18,24 +18,7 @@ const usePageDebriefing = (caseId?: Components.Schemas.Case["id"], id?: Componen
     [addSuccessFlashMessage, path, execPost]
   )
 
-  const handleUpdate = useCallback(payload =>
-    execPut(payload).then(() => {
-      addSuccessFlashMessage(path, "Succes", "De debriefing is succesvol gewijzigd")
-      return navigateTo(path)
-    }),
-    [addSuccessFlashMessage, path, execPut]
-  )
-
-  const handleDelete = useCallback(() =>
-    execDelete().then(() => {
-      addSuccessFlashMessage(path, "Succes", "De debriefing is succesvol verwijderd")
-      // TODO: Fix bug that debrief will be refetched, cause it's cache hook is used within displayed component
-      return navigateTo(path)
-    }),
-    [addSuccessFlashMessage, path, execDelete]
-  )
-
-  return { handleCreate, handleUpdate, handleDelete }
+  return { handleCreate }
 }
 
 export default usePageDebriefing
