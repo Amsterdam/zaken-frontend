15883f915417d662ea4b3484227b8b11f99db6b5
Remco Hoff <remco@code010.nl>
2021-03-18 14:57:21 +0100

    Cleanup unused MockComponents

--- src/__mocked__/apiSchema.d.ts
+++ src/__mocked__/apiSchema.d.ts
@@ -12,25 +12,5 @@ declare namespace MockComponents {
       readonly id: number
       readonly title: string
     }
-
-    export type Decision = {
-      readonly id: number
-      readonly title: string
-      isSanction?: boolean
-    }
-
-    export type Summon = {
-      readonly id: number
-      type: number
-      case: number
-      persons: SummonedPerson[]
-      readonly date_added: string // date-time
-      description?: string | null
-    }
-
-    export type DueDate = {
-      readonly id: number
-      readonly dueDate: string
-    }
   }
 }
