3881bfbe2a6319189ddb79af40dac9f311a6b300
Remco Hoff <remco@code010.nl>
2021-03-29 15:27:30 +0200

    Update TimelineEvents.tsx

--- src/app/components/cases/CaseTimeline/components/TimelineEvents.tsx
+++ src/app/components/cases/CaseTimeline/components/TimelineEvents.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import TimelineEvent, { TimelineEventItem } from "./TimelineEvent"
-import { TimelineWrapper } from "app/components/shared/Timeline"
 
 type Props = {
   items: TimelineEventItem[]
@@ -9,12 +8,11 @@ type Props = {
 const TimelineEvents: React.FC<Props> = ({ items }) => (
   <>
   { items.map((item, index) => (
-    <TimelineWrapper key={ item.caseEvents[0].id }>
-      <TimelineEvent
-        timelineEventItem={ item }
-        isOpen={ index === 0 }
-      />
-    </TimelineWrapper>
+    <TimelineEvent
+      key={ item.caseEvents[0].id }
+      timelineEventItem={ item }
+      isOpen={ index === 0 }
+    />
   )) }
   </>
 )
