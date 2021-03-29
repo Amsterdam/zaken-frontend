0e5bc9a9078c003d52205d4547a273b0fb88fdf0
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-03-29 14:56:42 +0200

    Fix date table header

--- src/app/components/cases/TableCases/TableCases.tsx
+++ src/app/components/cases/TableCases/TableCases.tsx
@@ -14,7 +14,7 @@ type Props = {
 const columns = [
   { header: "Adres", minWidth: 300 },
   { header: "Status", minWidth: 100 },
-  { header: "Laatst gewijzigd", minWidth: 100 },
+  { header: "Zaak aangemaakt", minWidth: 100 },
   { minWidth: 140 }
 ]
 
