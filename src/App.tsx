571430877083ce78708b90cb8a00139ac6ed06f5
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-04-02 15:01:59 +0200

    Set page titles

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import KeycloakProvider from "app/state/auth/keycloak/KeycloakProvider"
 import initializedCallback from "app/state/auth/keycloak/initializedCallback"
 import Router from "app/routing/components/Router"
 import FlashMessageProvider from "app/state/flashMessages/FlashMessageProvider"
-import ApiProvider from "./app/state/rest/provider/ApiProvider"
-import isLocalDevelopment from "./app/state/auth/keycloak/isLocalDevelopment"
+import ApiProvider from "app/state/rest/provider/ApiProvider"
+import isLocalDevelopment from "app/state/auth/keycloak/isLocalDevelopment"
 import { ErrorBoundary } from "@sentry/react"
+import PageTitle from "app/routing/components/PageTitle"
 
 const SentryErrorBoundary = process.env.REACT_APP_ENVIRONMENT !== undefined ? ErrorBoundary : Fragment
 
 const App = () => (
   <SentryErrorBoundary>
+    <PageTitle />
     <ThemeProvider>
       <GlobalStyle />
       <KeycloakProvider shouldInitialize={ isLocalDevelopment === false } initializedCallback={ initializedCallback }>
---
+++ src/app/routing/components/PageTitle.tsx
@@ -0,0 +1,27 @@
+import { FC, useEffect } from "react"
+import { globalHistory } from "@reach/router"
+import find from "../utils/find"
+import routes from "app/routing/routes"
+
+const PAGE_TITLE = process.env.REACT_APP_PAGE_TITLE ?? ""
+
+const setPageTitle = () => {
+  const route = find(routes, window.location.pathname)
+  const pageConfig = route ? routes[route] : undefined
+  const title = pageConfig?.title ? `${ pageConfig.title } | ${ PAGE_TITLE }` : PAGE_TITLE
+  document.title = title
+}
+
+const PageTitle: FC = () => {
+
+  useEffect(() => {
+    setPageTitle()
+    return globalHistory.listen(({ action }) => {
+      if (action !== "PUSH") return
+      setPageTitle()
+    })
+  }, [])
+
+  return null
+}
+export default PageTitle
