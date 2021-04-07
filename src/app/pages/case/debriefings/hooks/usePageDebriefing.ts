eed84bfe7e9bc408b0d5b8dddf5924a7670ad2a7
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-04-07 13:43:55 +0200

    Clean up cache hook

--- src/app/pages/case/debriefings/hooks/usePageDebriefing.ts
+++ src/app/pages/case/debriefings/hooks/usePageDebriefing.ts
@@ -4,8 +4,8 @@ import { useDebriefings } from "app/state/rest"
 import { useFlashMessages } from "app/state/flashMessages/useFlashMessages"
 import navigateTo from "app/routing/navigateTo"
 
-const usePageDebriefing = (caseId: Components.Schemas.Case["id"], id?: Components.Schemas.Debriefing["id"]) => {
-  const [, { execPost }] = useDebriefings(id, { lazy: true })
+const usePageDebriefing = (caseId: Components.Schemas.Case["id"]) => {
+  const [, { execPost }] = useDebriefings()
   const { addSuccessFlashMessage } = useFlashMessages()
 
   const handleCreate = useCallback(payload =>
