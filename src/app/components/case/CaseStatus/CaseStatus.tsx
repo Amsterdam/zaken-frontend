5f5fa4c91a590ab7b2d41875398d7e1db65d17b8
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-04-08 17:24:53 +0200

    TaskForm component

--- src/app/components/case/CaseStatus/CaseStatus.tsx
+++ src/app/components/case/CaseStatus/CaseStatus.tsx
@@ -1,11 +1,9 @@
 import { FC } from "react"
 import styled from "styled-components"
-import { Button, Divider, Heading, themeColor, themeSpacing } from "@amsterdam/asc-ui"
+import { Divider, Heading, themeSpacing } from "@amsterdam/asc-ui"
 
-import ButtonLink from "app/components/shared/ButtonLink/ButtonLink"
-import to from "app/routing/utils/to"
+import TaskForm from "./TaskForm"
 import Workflow from "../Workflow/Workflow"
-import AddTaskForm from "app/components/case/tasks/AddTask/AddTaskForm"
 
 type Props = {
   id: Components.Schemas.Case["id"]
@@ -16,12 +14,6 @@ const Div = styled.div`
   justify-content: space-between;
 `
 
-const StyledButton = styled(Button)`
-  color: ${ themeColor("tint", "level0") };
-  margin-left: ${ themeSpacing(2) };
-  margin-top: ${ themeSpacing(1) };
-`
-
 const StyledDivider = styled(Divider)`
   margin-bottom: ${ themeSpacing(16) };
 `
@@ -29,15 +21,8 @@ const StyledDivider = styled(Divider)`
 const CaseStatus: FC<Props> = ({ id }) => (
   <>
     <Div>
-      <Heading as="h2">
-        Status
-      </Heading>
-      <Div>
-        <AddTaskForm caseId={ id } />
-        <ButtonLink to={ to("/zaken/:id/afronden", { id }) }>
-          <StyledButton variant="tertiary">Afronden</StyledButton>
-        </ButtonLink>
-      </Div>
+      <Heading as="h2">Status</Heading>
+      <TaskForm id={ id } />
     </Div>
     <StyledDivider />
     <Workflow id={ id } />
