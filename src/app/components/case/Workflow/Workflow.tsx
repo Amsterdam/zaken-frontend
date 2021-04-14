a97219f2b5117fcbf26e039d083bcdcb8b930322
Remco Hoff <remco@code010.nl>
2021-04-14 17:23:29 +0200

    Copy change

--- src/app/components/case/Workflow/Workflow.tsx
+++ src/app/components/case/Workflow/Workflow.tsx
@@ -45,7 +45,7 @@ const mapArrayToList = (list: any[]) =>
 
 export const taskActionMap = {
   task_create_schedule: { name: "Huisbezoek inplannen", target: "inplanning" },
-  task_create_visit: { name: "Resultaat huisbezoek", target: "huisbezoek", disabled: true },
+  task_create_visit: { name: "Doorgeven huisbezoek TOP", target: "huisbezoek", disabled: true },
   task_create_debrief: { name: "Debrief verwerken", target: "debriefing" },
   task_create_summon: { name: "Aanschrijving verwerken", target: "aanschrijving" },
   task_create_decision: { name: "Besluit verwerken", target: "besluit" }
