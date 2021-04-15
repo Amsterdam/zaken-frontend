eb804dec89a1a440648d54c796a7ebda82286b03
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-04-15 12:53:05 +0200

    Fix useNavigateWithFlashMessage hook

--- src/app/state/flashMessages/useNavigateWithFlashMessage.ts
+++ src/app/state/flashMessages/useNavigateWithFlashMessage.ts
@@ -3,10 +3,12 @@ import to from "app/routing/utils/to"
 import navigateTo from "app/routing/navigateTo"
 
 type Level = "info" | "error"
-export default () => async (path: string, params?: Record<string, unknown>, level: Level = "info", title = "", body = "") => {
+export default () => {
   const { addSuccessFlashMessage, addErrorFlashMessage } = useFlashMessages()
-  const p = to(path, params)
-  const method = level === "error" ? addErrorFlashMessage : addSuccessFlashMessage
-  method(p, title, body)
-  navigateTo(p)
+  return async (path: string, params?: Record<string, unknown>, level: Level = "info", title = "", body = "") => {
+    const p = to(path, params)
+    const method = level === "error" ? addErrorFlashMessage : addSuccessFlashMessage
+    method(p, title, body)
+    navigateTo(p)
+  }
 }
