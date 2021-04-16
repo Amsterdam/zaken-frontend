e5b99d78015f05e899ab8baf1bab730e81b7e3dd
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-04-16 13:12:41 +0200

    Move CaseCompleteForm

--- src/app/components/case/CaseCompleteForm/CaseCompleteForm.tsx
+++ src/app/components/case/CaseCompleteForm/CaseCompleteForm.tsx
@@ -3,7 +3,7 @@ import { FormTitle } from "@amsterdam/asc-ui"
 
 import { useCompleteCase } from "app/state/rest/"
 import WorkflowForm from "app/components/case/Workflow/WorkflowForm"
-import scaffold from "app/components/cases/CaseCompleteForm/scaffold"
+import scaffold from "app/components/case/CaseCompleteForm/scaffold"
 import useScaffoldedFields from "app/components/shared/ConfirmScaffoldForm/hooks/useScaffoldedFields"
 
 type Props = {
