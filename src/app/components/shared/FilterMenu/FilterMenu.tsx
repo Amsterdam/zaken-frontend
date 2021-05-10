5a6d66380c331b7d13081e2cc2d276503d1be196
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-05-10 13:56:53 +0200

    Use div vs. menu

--- src/app/components/shared/FilterMenu/FilterMenu.tsx
+++ src/app/components/shared/FilterMenu/FilterMenu.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import { themeColor, themeSpacing } from "@amsterdam/asc-ui"
 
-export default styled.menu`
+export default styled.div`
   background: ${ themeColor("tint", "level2") };
   margin-top: ${ themeSpacing(8) };
   padding: ${ themeSpacing(4) } ${ themeSpacing(6) };
