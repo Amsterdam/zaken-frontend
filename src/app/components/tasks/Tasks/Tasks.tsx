fc9baf5e9c78fb64f176def2177ea29e4c3c14d3
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-05-11 15:17:42 +0200

    Remove unnecessary useEffect

--- src/app/components/tasks/Tasks/Tasks.tsx
+++ src/app/components/tasks/Tasks/Tasks.tsx
@@ -1,21 +1,15 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
+import { useRoles, useTasks } from "app/state/rest"
 import { Row, Column } from "app/components/layouts/Grid"
 import TableTasks from "app/components/tasks/TableTasks/TableTasks"
 import TasksFilter from "../TasksFilter/TasksFilter"
-import { useRoles, useTasks } from "app/state/rest"
 
 const Tasks: React.FC = () => {
 
   const [roles] = useRoles()
   const [role, setRole] = useState<MockComponents.Schemas.Role>("")
   const [tasks, { isBusy }] = useTasks(role)
-
-  useEffect(() => {
-    if (role !== undefined) return
-    if (roles === undefined) return
-    setRole("")
-  }, [role, roles, setRole])
 
   return (
     <Row>
