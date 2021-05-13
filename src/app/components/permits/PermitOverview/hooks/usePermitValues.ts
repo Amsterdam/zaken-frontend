cafc9136631af5a7c4f2c51f672a07b7946e29fa
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-05-13 12:17:18 +0200

    Uppercase Ja, Nee

--- src/app/components/permits/PermitOverview/hooks/usePermitValues.ts
+++ src/app/components/permits/PermitOverview/hooks/usePermitValues.ts
@@ -6,9 +6,9 @@ export default (data?: Components.Schemas.Decos) => (
       const permits = data?.permits.filter(({ permit_granted }) => ["GRANTED", "NOT_GRANTED"].includes(permit_granted))
       if (permits === undefined || permits.length === 0) return undefined
       return permits.reduce((acc, { permit_type, permit_granted }) => {
-        acc[permit_type] = permit_granted === "GRANTED" ? "ja" : "nee"
+        acc[permit_type] = permit_granted === "GRANTED" ? "Ja" : "Nee"
         return acc
-      }, {} as Record<string, "ja" | "nee">)
+      }, {} as Record<string, string>)
     }, [data]
   )
 )
