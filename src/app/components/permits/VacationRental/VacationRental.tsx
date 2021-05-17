8d8c0f4652677d2d57f17b326c2ade438d57c03a
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-05-17 11:35:41 +0200

    Add heading for "Vakantieverhuur meldingen"

--- src/app/components/permits/VacationRental/VacationRental.tsx
+++ src/app/components/permits/VacationRental/VacationRental.tsx
@@ -1,4 +1,4 @@
-import { Spinner, Paragraph } from "@amsterdam/asc-ui"
+import { Spinner, Paragraph, Heading } from "@amsterdam/asc-ui"
 
 import { usePermitDetails } from "app/state/rest"
 import TwoColumns from "./components/TwoColumns"
@@ -23,33 +23,37 @@ const VacationRental: React.FC<Props> = ({ bagId }) => {
     <>
       { isBusy ?
         <Spinner /> :
-        reports === undefined ?
-        <Paragraph>Geen vakantieverhuur meldingen</Paragraph> :
-        <Grid>
-          <Label>Nachten verhuurd</Label>
-          <Text>
-            <strong>{ reports.rented_days_count }</strong>
-          </Text>
-          <Label>Vandaag verhuurd</Label>
-          <Text>{ reports.is_rented_today ? "Ja" : "Nee" }</Text>
-          <Label>Nachten gepland</Label>
-          <Text>{ reports.planned_days_count }</Text>
-          { (reports.rented_days_count ?? 0) > 0 &&
-          <>
-            <TwoColumns>
-              <Hr />
-            </TwoColumns>
-            <Details>
-              <Summary>Alle meldingen</Summary>
-              <Grid>
-                { reports.meldingen.map(({ check_in_date, check_out_date, is_afmelding }, index: number) =>
-                    <VacationRentalReport checkInDate={ check_in_date } checkOutDate={ check_out_date } isAfmelding={ is_afmelding } key={ index } />
-                ) }
-              </Grid>
-            </Details>
-          </>
+        <>
+          <Heading forwardedAs="h4">Vakantieverhuur meldingen</Heading>
+          { reports === undefined ?
+            <Paragraph>Geen vakantieverhuur meldingen</Paragraph> :
+            <Grid>
+              <Label>Nachten verhuurd</Label>
+              <Text>
+                <strong>{ reports.rented_days_count }</strong>
+              </Text>
+              <Label>Vandaag verhuurd</Label>
+              <Text>{ reports.is_rented_today ? "Ja" : "Nee" }</Text>
+              <Label>Nachten gepland</Label>
+              <Text>{ reports.planned_days_count }</Text>
+              { (reports.rented_days_count ?? 0) > 0 &&
+              <>
+                <TwoColumns>
+                  <Hr />
+                </TwoColumns>
+                <Details>
+                  <Summary>Alle meldingen</Summary>
+                  <Grid>
+                    { reports.meldingen.map(({ check_in_date, check_out_date, is_afmelding }, index: number) =>
+                        <VacationRentalReport checkInDate={ check_in_date } checkOutDate={ check_out_date } isAfmelding={ is_afmelding } key={ index } />
+                    ) }
+                  </Grid>
+                </Details>
+              </>
+              }
+            </Grid>
           }
-        </Grid>
+        </>
       }
     </>
   )
