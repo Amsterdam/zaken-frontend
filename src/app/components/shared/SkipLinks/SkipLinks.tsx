5c0a45ca2db39ed5ac89f82fab66183834ea35e1
Remco Hoff <remco@code010.nl>
2021-05-17 15:26:41 +0200

    Little refactoring

--- src/app/components/shared/SkipLinks/SkipLinks.tsx
+++ src/app/components/shared/SkipLinks/SkipLinks.tsx
@@ -18,7 +18,7 @@ const StyledList = styled(List)`
 const StyledLink = styled(Link)`
 
     background-color: ${ themeColor("primary") };
-    color: rgb(255, 255, 255);
+    color: ${ themeColor("tint", "level1") };
     left: 50%;
     padding: 12px 16px;
     position: absolute;
@@ -26,6 +26,9 @@ const StyledLink = styled(Link)`
     transition: transform 0.3s ease 0s;
     z-index: -999;
 
+    &:hover {
+      color: ${ themeColor("tint", "level1") };
+    }
     &:focus {
       transform: translate(-50%, 0%);
       z-index: 999;
