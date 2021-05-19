e154a33a802db0c6a8e31d8bad240252cf337756
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-05-19 17:37:03 +0200

    Keycloak

---
+++ src/app/components/auth/KeycloakValues/hooks/useValues.ts
@@ -0,0 +1,15 @@
+import type KeycloakTokenParsedExtended from "app/state/auth/keycloak/KeycloakTokenParsedExtended"
+import { KeycloakInstance } from "keycloak-js"
+
+export default (keycloak?: KeycloakInstance, token?: KeycloakTokenParsedExtended) => {
+  if (keycloak === undefined || token === undefined) return
+
+  const values = [
+    ["Naam", token.name],
+    ["E-mail", token.email],
+    ["Gebruikersnaam", token.preferred_username],
+    ["Keycloak groepen", keycloak.realmAccess?.roles.join(", ") ?? "-"]
+  ]
+
+  return Object.fromEntries(values)
+}
