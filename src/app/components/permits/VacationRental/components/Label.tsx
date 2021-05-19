3f18c81be952ad50a4ab5fe54ce88cb6c6ee7a30
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-05-19 15:31:35 +0200

    DefinitionList VacationRental

--- src/app/components/permits/VacationRental/components/Label.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-import { themeColor } from "@amsterdam/asc-ui"
-
-export default styled.label`
-  color: ${ themeColor("tint", "level5") };
-  font-weight: 500;
-  word-break: break-word;
-`
