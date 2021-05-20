bde37c389616410c0113d16b7355e6fa10795356
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-05-20 10:43:36 +0200

    CaseDetails

--- src/app/components/case/CaseDetails/CaseDetails.tsx
+++ src/app/components/case/CaseDetails/CaseDetails.tsx
@@ -1,29 +1,23 @@
-import { useMemo } from "react"
-import { CaseIdDisplay, displayDate } from "@amsterdam/wonen-ui"
-
 import DefinitionList from "app/components/shared/DefinitionList/DefinitionList"
 import { useCase } from "app/state/rest"
+import useValues from "./hooks/useValues"
 
 type Props = {
   caseId: Components.Schemas.Case["id"]
 }
 
-const mapData = (data: Components.Schemas.Case) => ({
-  "Zaak ID": <CaseIdDisplay id={ data.id } />,
-  "Team": data.team.name,
-  "Startdatum": data.start_date ? `${ displayDate(data.start_date) }` : "-"
-})
-
 const CaseDetails: React.FC<Props> = ({ caseId }) => {
-  const [data] = useCase(caseId)
-  const values = useMemo(() => data !== undefined ? mapData(data) : {}, [data])
 
-  return <DefinitionList
-    isLoading={ data === undefined }
-    numInitialVisibleRows={ 3 }
-    numLoadingRows={3}
-    values={ values }
-  />
+  const [data, { isBusy }] = useCase(caseId)
+  const values = useValues(data)
+
+  return (
+    <DefinitionList
+      isLoading={ isBusy }
+      numLoadingRows={ 3 }
+      values={ values }
+    />
+  )
 }
 
 export default CaseDetails
