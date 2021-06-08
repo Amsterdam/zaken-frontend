69f6398ed0a68dbac87b8b30d059678b09d154ab
Remco Hoff <remco@code010.nl>
2021-06-08 15:22:05 +0200

    Cleanup code

--- src/app/translations/translationsViolationTypes.ts
+++ src/app/translations/translationsViolationTypes.ts
@@ -3,7 +3,6 @@ const translationsViolationTypes: Record<string, string> = {
     "NO": "Geen overtreding",
     "ADDITIONAL_RESEARCH_REQUIRED": "Nader intern onderzoek nodig",
     "ADDITIONAL_VISIT_REQUIRED": "Aanvullend bezoek nodig",
-    "AUTHORIZATION_REQUEST": "Aanvraag machtiging",
     "ADDITIONAL_VISIT_WITH_AUTHORIZATION": "Nieuw bezoek inclusief machtigingsaanvraag",
     "SEND_TO_OTHER_THEME": "Naar ander thema"
   }
