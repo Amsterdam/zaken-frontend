3bcab9b70228541cc583eaccfd9bf1b71ffa7667
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-06-10 14:48:01 +0200

    Tab indention

--- src/app/components/case/forms/DecisionForm/utils/stripThousandSeparator.test.ts
+++ src/app/components/case/forms/DecisionForm/utils/stripThousandSeparator.test.ts
@@ -2,15 +2,15 @@ import stripThousandSeparator from "./stripThousandSeparator"
 
 describe("stripThousandSeparator", () => {
 
-	it("stripThousandSeparator for thousands", () => {
+  it("stripThousandSeparator for thousands", () => {
     expect(stripThousandSeparator("1.000.000")).toBe("1000000")
-	})
+  })
 
-	it("stripThousandSeparator for non thousands", () => {
+  it("stripThousandSeparator for non thousands", () => {
     expect(stripThousandSeparator("1.00")).toBe("1.00")
-	})
+  })
 
-	it("separator", () => {
+  it("separator", () => {
     expect(stripThousandSeparator("1,000,000", ",")).toBe("1000000")
-	})
+  })
 })
