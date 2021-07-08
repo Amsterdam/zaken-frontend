bc026501caa67f85212274df0b338626146a7ee3
Remco Hoff <remco@code010.nl>
2021-07-08 13:21:14 +0200

    Refactoring CaseHeader (removed filter)

--- src/app/components/case/CaseDetails/hooks/useValues.tsx
+++ src/app/components/case/CaseDetails/hooks/useValues.tsx
@@ -11,9 +11,8 @@ export default (caseItem?: Components.Schemas.Case) => {
     ["Zaak ID", <CaseIdDisplay id={ id } />],
     ["Thema", name],
     ["Startdatum", <DateDisplay date={ start_date ?? undefined } emptyText="-" />],
-    ["Aanleiding", reason.name],
-    project?.name !== undefined ? ["Project", project.name] : undefined
-  ].filter((value): value is [string, React.ReactElement] => value !== undefined)
+    ["Aanleiding", project?.name !== undefined ? `Project: ${ project.name }` : reason.name]
+  ]
 
   return Object.fromEntries(values)
 }
