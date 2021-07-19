11815d5a9407145f007cabbf3f02fe7253bd7843
Remco Hoff <remco@code010.nl>
2021-07-19 16:22:27 +0200

    Add second mapping for task_close_case

--- src/app/components/case/Workflow/utils/taskActionMap.ts
+++ src/app/components/case/Workflow/utils/taskActionMap.ts
@@ -11,6 +11,8 @@ const taskActionMap: Record<string, TaskAction> = {
   task_create_summon: { name: "Aanschrijving verwerken", target: "aanschrijving" },
   task_create_decision: { name: "Besluit verwerken", target: "besluit" },
   task_create_signal: { name: "Melding verwerken", target: "melding" },
+  task_close_case: { name: "Zaak afsluiten", target: "afronding" },
+  // TODO 19-07-2021 remove next line when it is sure no older camunda-processes use "create_case_closing" anymore
   create_case_closing: { name: "Zaak afsluiten", target: "afronding" }
 }
 
