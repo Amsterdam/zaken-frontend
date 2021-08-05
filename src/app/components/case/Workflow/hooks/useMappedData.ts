638d006444bd4b7807894ff881fe61ab0d6cd6ff
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-08-05 10:38:33 +0200

    Remove unecessary useMemo

--- src/app/components/case/Workflow/hooks/useMappedData.ts
+++ src/app/components/case/Workflow/hooks/useMappedData.ts
@@ -1,12 +1,7 @@
-import { useMemo } from "react"
 import mapTaskData from "../utils/mapTaskData"
 
-
 export default (id: Components.Schemas.Case["id"], data: Components.Schemas.CamundaTaskWithState[] | undefined, execPost: () => Promise<unknown>) =>
-  useMemo(
-    () => data?.map(
-      ({ state: { status_name, information }, tasks }) =>
-        [status_name, information, tasks.map(mapTaskData(id, execPost))] as const
-    ),
-    [data, id, execPost]
+  data?.map(
+    ({ state: { status_name, information }, tasks }) =>
+      [status_name, information, tasks.map(mapTaskData(id, execPost))] as const
   )
