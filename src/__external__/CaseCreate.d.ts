c54e078a4f59cfbab9f86e9a835afe9b6b16c2a8
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-08-24 14:13:22 +0200

    Clean up CaseCreate type

--- src/__external__/CaseCreate.d.ts
+++ src/__external__/CaseCreate.d.ts
@@ -1,3 +1,4 @@
+// We override the address key here. Because the generated type expects a complete Address instead of just a bag_id
 declare type CaseCreate = Omit<Components.Schemas.CaseCreateUpdate, "id" | "address"> &
-  { address: { bag_id: Components.Schemas.Address["bag_id"] }, ton_ids?: number[] | undefined }
+  { address: { bag_id: Components.Schemas.Address["bag_id"] } }
 
