533e731a966bbdaf5102cccd244f0e0fb13c0db4
Remco Hoff <remco@code010.nl>
2021-09-22 13:56:24 +0200

    Addcase flow with errors

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -88,3 +88,32 @@ Cypress.Commands.add("loginAsPm", () => {
 Cypress.Commands.add("loginAsTh", () => {
   cy.login(Cypress.env("userTh"), Cypress.env("TEST_USER_PASSWORD"))
 })
+
+// Check for empty textField
+Cypress.Commands.add("checkRequiredField", ( field, validInput="test", errorMessage="Dit veld is verplicht") => {
+  cy.get(field)
+    .focus()
+    .blur()
+    .siblings()
+    .contains(errorMessage)
+
+  cy.get(field)
+    .type(validInput)
+    .siblings()
+    .should("not.exist")
+})
+
+// Check for wrong input
+Cypress.Commands.add("checkInvalidInput", ( field, errorMessage, validInput, invalidInput="test") => {
+  cy.get(field)
+  .type(invalidInput)
+    .blur()
+    .siblings()
+    .contains(errorMessage)
+
+  cy.get(field)
+    .clear()
+    .type(validInput)
+    .siblings()
+    .should("not.exist")
+})
