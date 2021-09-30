2f61e8be7f606e762a5a576ea94f4ad4aa0e0b7d
Remco Hoff <remco@code010.nl>
2021-09-30 11:54:51 +0200

    Finalising flow Debrief No Violation

--- cypress/integration/040_debrief/debrief.no.violation.spec.js
+++ cypress/integration/040_debrief/debrief.no.violation.spec.js
@@ -27,7 +27,7 @@ describe('Process Debrief - No violation"', () => {
         .click()
     })
 
-    it('Intercept TOP URL and load page', () => {
+    it('Intercept Debrief URL and load page', () => {
 
       const url = `${Cypress.env("baseUrlAcc")}cases/*/tasks/`
       cy.intercept(url).as('getTasks')
@@ -59,7 +59,7 @@ describe('Process Debrief - No violation"', () => {
 
     it('Type a note', () => {
       cy.get('[data-e2e-id="feedback"]')
-      .type(debrief.descriptionNoViolation)
+      .type(debrief.noViolationDescription)
     })
 
     it('Submit form and check debrief status', () => {
@@ -74,7 +74,7 @@ describe('Process Debrief - No violation"', () => {
 
       cy.get(`[role="dialog"]`)
         .should("contain", debrief.labelNo)
-        .and("contain", debrief.descriptionNoViolation)
+        .and("contain", debrief.noViolationDescription)
         .find(`button`)
         .contains(debrief.formButtonText)
         .click()
@@ -146,8 +146,9 @@ describe('Process Debrief - No violation"', () => {
 
       cy.get(`[role="dialog"]`)
         .should('have.length', 1)
-        .find("h4")
-        .should("contain", "Opstellen verkorte rapportage huisbezoek")
+        .contains(debrief.noViolationNextTask2)
+        
+      cy.get(`[role="dialog"]`)
         .find('input[name="completed"]')
         .first()
         .check()
@@ -158,29 +159,116 @@ describe('Process Debrief - No violation"', () => {
         .click()
     })
 
-    it("Check debrief event in history", () => {
+    it("Check next task is 'Uitzetten vervolgstap'", () => {
       const url = `${Cypress.env("baseUrlAcc")}cases/*/tasks/`
       cy.intercept(url).as('getNextTask')
 
       cy.wait('@getNextTask').then(() => {
-        cy.get("h2")
-        .contains("Zaakhistorie")
-      cy.get(`button[title="${debrief.noViolationNextTask2} "]`)
-        .should("have.attr", "aria-expanded", "true")
-        .contains(debrief.noViolationNextTask2)
+          
+        cy.scrollTo(0, 400)
+        cy.get("h4")
+          .contains("Vervolgstap")
+        cy.get("tbody>tr")
+          .contains("td", debrief.closingTask1)
+          .siblings("td")
+          .contains(roles.PHH)
+          .parents('td')
+          .siblings('td')
+          .contains("Taak afronden")
       })
     })
 
-    it("Check id next task is 'Zaak afsluiten'", () => {
-      const url = `${Cypress.env("baseUrlAcc")}cases/*/tasks/`
-      cy.intercept(url).as('getNextTask')
+    it("Check debrief event in history", () => {
+      cy.scrollTo(0, 600)
+      cy.get("h2")
+        .contains("Zaakhistorie")
+      // TODO when double spaces in bpmn are fixed in the backend
+      // use debrief.noViolationNextTask2
+      cy.get(`button[title="Opstellen  verkorte rapportage huisbezoek "]`)
+        .should("have.attr", "aria-expanded", "true")
+        .contains(debrief.noViolationNextTask2)
+    })
 
-      cy.wait('@getNextTask').then(() => {
+    it("Select to close this case", () => {
+      cy.get('button')
+        .contains("Taak afronden")
+        .click({force: true})
+
+      cy.get(`[role="dialog"]`)
+        .should('have.length', 1)
+        .and("contain", debrief.closingTask1)
+      cy.get('[data-e2e-id="next_step"]')
+        .select(debrief.closingTask2)
+
+      cy.get(`[data-e2e-id="submit"]`)
+        .click()
+    })
+
+    it('Intercept Afronding URL and load page', () => {
+
+      const url = `${Cypress.env("baseUrlAcc")}cases/*/tasks/`
+      cy.intercept(url).as('getTasks')
+
+      cy.wait('@getTasks').then(({ response }) => {
+        const closingResponse = response?.body?.find((e) => e.state?.status_name === debrief.closingTask2)
+        const caseId = closingResponse?.state?.case
+        const closingTask = closingResponse?.tasks?.find((e) => e.name === debrief.closingTask2)
+        const taskId = closingTask.camunda_task_id
+
+        const url = `${Cypress.env("baseUrlAcc")}themes/58/case-close-reasons`
+        cy.intercept(url).as('getCloseReasons')
+        cy.get("h4")
+          .contains(debrief.closingTask2)
         cy.get("tbody>tr")
+          .contains("td", debrief.closingTask2)
+          .siblings("td")
           .contains(roles.PM)
           .parents('td')
           .siblings('td')
           .contains("Zaak afsluiten")
+
+        cy.visit(`/zaken/${caseId}/afronding/${taskId}`)
+
+        cy.wait("@getCloseReasons").then(() => {
+          cy.get("h1")
+            .contains("Zaak afronden")
+        })
+      })
+    })
+
+    it("Fill in the form", () => {
+      cy.get("label[for='reason_3']")
+        .find('[data-e2e-id="3"]')
+        .check({force: true})
+
+        cy.get('[data-e2e-id="description"]')
+          .type(debrief.closingDescription)
+
+        cy.get('button[data-e2e-id="submit"]')
+          .contains("Verwerken")
+          .click()
+    })
+
+    it("Submit the form", () => {
+      cy.get(`[role="dialog"]`).should('have.length', 1)
+    
+      cy.get(`[role="dialog"]`)
+        .should("contain", debrief.closingReason3)
+        .and("contain", debrief.closingDescription)
+        .find(`button`)
+        .contains("Verwerken")
+        .click()
+    })
+
+    it("Request is successfully processed", () => {
+      const url = `${Cypress.env("baseUrlAcc")}cases/*/events/`
+      cy.intercept(url).as('getEvents')
+      cy.wait('@getEvents').then(() => {
+        cy.get("h1").contains("Zaakdetails")
+        cy.get("h2").contains("Zaakhistorie")
+        cy.get("h2").contains("Status").should("not.exist")
+        cy.get("span").contains("Zaak afgerond ")
+        cy.get("span").contains("Uitzetten vervolgstap ")
       })
     })
   })
