104578415b6721cd6f7593a4708e672b93583eb4
Remco Hoff <remco@code010.nl>
2021-10-05 11:58:48 +0200

    Only PM is authorised to close cases

---
+++ cypress/integration/060_closeCase/3.close.case.toezichthouder.spec.js
@@ -0,0 +1,39 @@
+import address from "../../fixtures/address.json"
+
+describe('Select Next Step - closing case as toezichthouder', () => {
+
+  it("Logout", () => {
+    cy.logout()
+  })
+
+  it("Login as toezichthouder", () => {
+    cy.loginAsTh()
+  })
+
+  it("Go to Adresoverzicht and check address", () => {
+    const url = `${Cypress.env("baseUrlAcc")}addresses/*/cases/?open_cases=true`
+    cy.intercept(url).as('getCases')
+    cy.visit(`/adres/${address.bagId}`)
+    cy.wait('@getCases').then(() => {
+      cy.get("h1")
+        .contains(`${address.street}, ${address.zipCode}`)
+    })
+  })
+
+  it("Adresoverzicht has right address", () => {
+    cy.get("h1")
+      .contains(`${address.street}, ${address.zipCode}`)
+  })
+
+  it('Get first case with task "Afsluiten zaak"', () => {
+    cy.scrollTo(0, 400)
+    cy.get("tbody>tr")
+      .contains("td", "Afsluiten zaak")
+      .click()
+  })
+
+  it("Keycloak authorisation error should be visible", () => {
+    cy.get("h2")
+      .contains("Keycloak gebruiker")
+  })
+})
--- cypress/integration/060_closeCase/4.close.case.no.violation.spec.js
+++ cypress/integration/060_closeCase/4.close.case.no.violation.spec.js
@@ -1,8 +1,38 @@
 import debrief from "../../fixtures/debrief.json"
 import roles from "../../fixtures/roles.json"
+import address from "../../fixtures/address.json"
 
-describe('Select Next Step - closing case"', () => {
+describe('Close case as projectmedwerker"', () => {
 
+  it("Logout", () => {
+    cy.logout()
+  })
+  
+  it("Login as projectmedewerker", () => {
+    cy.loginAsPm()
+  })
+
+  it("Go to Adresoverzicht and check address", () => {
+    const url = `${Cypress.env("baseUrlAcc")}addresses/*/cases/?open_cases=true`
+    cy.intercept(url).as('getCases')
+    cy.visit(`/adres/${address.bagId}`)
+    cy.wait('@getCases').then(() => {
+      cy.get("h1")
+        .contains(`${address.street}, ${address.zipCode}`)
+    })
+  })
+
+  it("Adresoverzicht has right address", () => {
+    cy.get("h1")
+      .contains(`${address.street}, ${address.zipCode}`)
+  })
+
+  it('Get first case with task "Afsluiten zaak"', () => {
+    cy.scrollTo(0, 400)
+    cy.get("tbody>tr")
+      .contains("td", "Afsluiten zaak")
+      .click()
+  })
   it('Intercept Afronding URL and load page', () => {
 
     const url = `${Cypress.env("baseUrlAcc")}cases/*/tasks/`
