f68ca2dd70afaefa65300db9117a37c0d720f791
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2021-10-14 14:38:03 +0200

    Added flex prop to buttonLink

--- src/app/components/shared/TableAction/TableAction.tsx
+++ src/app/components/shared/TableAction/TableAction.tsx
@@ -38,7 +38,7 @@ const TableAction: React.FC<Props> = ({ to, disabled = false, title, children, .
 
   return (
     isLink ? (
-      <ButtonLink to={ to! } onClick={ onClick }>
+      <ButtonLink to={ to! } onClick={ onClick } flex>
         { action }
       </ButtonLink>
     ) : action
