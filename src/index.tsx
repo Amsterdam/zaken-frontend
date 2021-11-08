b6c7faf41daff23d86a044f4f4116b152f46bb57
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2021-11-08 15:14:21 +0100

    Logging version in console

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
+import { name, version } from "../package.json"
 
 // Sentry
 import initSentry from "./app/sentry/init"
@@ -21,4 +22,5 @@ ReactDOM.render(
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
 
+console.log("Name:", name, version)
 console.log("Commit hash:", process.env.REACT_APP_GIT_COMMIT_HASH ?? "n/a")
