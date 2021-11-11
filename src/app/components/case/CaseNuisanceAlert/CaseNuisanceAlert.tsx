8768f97461565051aa235595ba7a9073cd02dcd7
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2021-11-11 15:39:55 +0100

    Removed console.log

--- src/app/components/case/CaseNuisanceAlert/CaseNuisanceAlert.tsx
+++ src/app/components/case/CaseNuisanceAlert/CaseNuisanceAlert.tsx
@@ -20,7 +20,6 @@ const CaseNuisanceAlert: React.FC<Props> = ({ caseId }) => {
   const totalNuisance = caseEvents?.reduce((acc, cur) => cur?.event_values?.nuisance ? ++acc : acc, 0)
   const isMaxExceeded = totalNuisance !== undefined && totalNuisance >= MAX_NUMBER_NUISANCE
   const isNuisanceReportedInStates = caseData?.current_states.find((state) => state.status_name === "Melding overlast")
-  console.log("CASE EVENTS", caseEvents)
   const isNuisanceReportedInEvents = caseEvents?.find((event) => event?.event_values?.description === "Doorzetten melding overlast")
 
   const isVisible = isMaxExceeded && !isNuisanceReportedInStates && !isNuisanceReportedInEvents
