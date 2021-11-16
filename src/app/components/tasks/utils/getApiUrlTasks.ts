4118eea9a2bfec1cc38c1cbf0a6980c386c786e0
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2021-11-16 12:43:23 +0100

    Adjusted filter context url

--- src/app/components/tasks/utils/getApiUrlTasks.ts
+++ src/app/components/tasks/utils/getApiUrlTasks.ts
@@ -1,11 +1,16 @@
 import { makeApiUrl } from "app/state/rest/hooks/utils/apiUrl"
 
+const translatedKeys: Record<string, string> = {
+  rol: "role",
+  thema: "theme"
+}
+
 /**
  * Utility function to create an API URL for tasks.
  * This is used to call the tasks context that is saved by apiUrl.
  */
 export default () => {
   const dutchApiUrl = `${ makeApiUrl("tasks") }${ window.location.search }`
-  const apiUrlTasks = dutchApiUrl.replace("rol", "role")
+  const apiUrlTasks = dutchApiUrl.replace(/rol|thema/gi, (matched: string) => translatedKeys[matched])
   return apiUrlTasks
 }
