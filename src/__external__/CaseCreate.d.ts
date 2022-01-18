5ad4a6aa029ed61c922775cb11eedc795fc3f2f2
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2022-01-18 22:56:59 +0100

    Updated types and create case

--- src/__external__/CaseCreate.d.ts
+++ src/__external__/CaseCreate.d.ts
@@ -1,4 +1,13 @@
-// We override the address key here. Because the generated type expects a complete Address instead of just a bag_id
-declare type CaseCreate = Omit<Components.Schemas.CaseCreateUpdate, "id" | "address"> &
-  { address: { bag_id: Components.Schemas.Address["bag_id"] }, nuisance: boolean }
+type CitizenReport = {
+  identification: Components.Schemas.CitizenReportCase["identification"]
+  nuisance: Components.Schemas.CitizenReportCase["nuisance"]
+}
 
+// We override the address key here. Because the generated type expects a complete Address instead of just a bag_id
+declare type CaseCreate = Omit<Components.Schemas.Case, "id" | "address" | "current_states" | "schedules" | "last_updated" | "created"> &
+  {
+    address: {
+      bag_id: Components.Schemas.Address["bag_id"]
+    }
+    citizen_reports: Omit<Components.Schemas.CitizenReportCase, "id" | "date_added">[]
+  }
