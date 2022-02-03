974c1e417b725412b9f5b8235e43d7af0a2a3a84
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2022-02-03 14:59:23 +0100

    Added translation LIKELY_INHABITED

--- src/app/translations/translationsViolationTypes.ts
+++ src/app/translations/translationsViolationTypes.ts
@@ -4,8 +4,8 @@ const translationsViolationTypes: Record<string, string> = {
     "ADDITIONAL_RESEARCH_REQUIRED": "Nader intern onderzoek nodig",
     "ADDITIONAL_VISIT_REQUIRED": "Aanvullend bezoek nodig",
     "ADDITIONAL_VISIT_WITH_AUTHORIZATION": "Machtiging benodigd",
-    "SEND_TO_OTHER_THEME": "Naar ander thema"
+    "SEND_TO_OTHER_THEME": "Naar ander thema",
+    "LIKELY_INHABITED": "Vermoeden bewoning"
   }
-  
+
   export default translationsViolationTypes
-  
