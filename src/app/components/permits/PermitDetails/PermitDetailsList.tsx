9828cc796e351d04d2391250f78f18073d314d74
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2022-02-07 18:34:53 +0100

    Spaces as tab

--- src/app/components/permits/PermitDetails/PermitDetailsList.tsx
+++ src/app/components/permits/PermitDetails/PermitDetailsList.tsx
@@ -6,34 +6,34 @@ import useKnownPermits from "./hooks/useKnownPermits"
 import PermitDetails from "./PermitDetails"
 
 type Props = {
-	bagId: string
+  bagId: string
 }
 
 const PermitDetailsList: React.FC<Props> = ({ bagId }) => {
-	const [data, { isBusy }] = usePermitDetails(bagId)
-	const permits = useKnownPermits(data)
+  const [data, { isBusy }] = usePermitDetails(bagId)
+  const permits = useKnownPermits(data)
 
-	if (isBusy) {
-		return <Spinner />
-	}
-	return (
-		<>
-			{ permits === undefined ? (
-					<>
-						<Heading forwardedAs="h4">Vergunningen</Heading>
-						<Paragraph>Geen vergunningen gevonden</Paragraph>
-					</>
-				) : (
-					<div>
-						{ permits.map(permit => (
-								<PermitDetails key={ permit.permit_type } permit={ permit } />
-							)
-						)}
-					</div>
-				)
-			}
-		</>
-	)
+  if (isBusy) {
+    return <Spinner />
+  }
+  return (
+    <>
+      { permits === undefined ? (
+          <>
+            <Heading forwardedAs="h4">Vergunningen</Heading>
+            <Paragraph>Geen vergunningen gevonden</Paragraph>
+          </>
+        ) : (
+          <div>
+            { permits.map(permit => (
+                <PermitDetails key={ permit.permit_type } permit={ permit } />
+              )
+            )}
+          </div>
+        )
+      }
+    </>
+  )
 }
 
 export default PermitDetailsList
