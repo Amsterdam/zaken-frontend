9dff01c6de5014bdbe2c74074ada5b0e91983d90
Maurice Guikema <mguikema@gmail.com>
2022-03-10 10:42:01 +0100

    feature: checkbox type boolean for workflow forms, lint fixes

--- src/app/components/case/tasks/WorkflowTask/utils/mapSubmitData.ts
+++ src/app/components/case/tasks/WorkflowTask/utils/mapSubmitData.ts
@@ -3,7 +3,7 @@ import { appendTimeToDate } from "app/components/shared/Helpers/helpers"
 // type Rec = Record<string, { value: string | number | boolean }>
 
 const castValue = (item: any, value: any) => {
-  if (item.type === "select") return value.value != "" ? value.value : null
+  if (item.type === "select") return value.value !== "" ? value.value : null
   if (item.type === "checkbox") return !!(value ?? false)
   if (item.camunda_type === "Long") return parseFloat(value)
   if (item.is_date) return appendTimeToDate(value)
--- src/app/components/case/tasks/WorkflowTask/utils/mapWorkflowDataToInitialValues.ts
+++ src/app/components/case/tasks/WorkflowTask/utils/mapWorkflowDataToInitialValues.ts
@@ -1,7 +1,7 @@
 type Rec = Record<string, Array<string> | string | { label: string, value: string }>
 export default (workflowForm: Components.Schemas.CaseUserTaskWorkdflow["form"]) => (
   workflowForm.reduce((acc: Rec, item: any) => {
-    const { default_value, label, name, type } = item
+    const { default_value, name, type } = item
     // The API response is somewhat inconsistent
     // default_value == "false" means checked
     // default_value == "" means unchecked
