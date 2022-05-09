5b35fbc016552b84a045d67329e653c36271d7f7
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2022-05-09 14:43:14 +0200

    Updated cypress test

--- cypress/integration/010_addCase/themes/vakantieverhuur/add.case.anonymous.no.ad.spec.js
+++ cypress/integration/010_addCase/themes/vakantieverhuur/add.case.anonymous.no.ad.spec.js
@@ -96,6 +96,6 @@ describe("Create case and validate input", () => {
   })
 
   it("History contains the right items", () => {
-    cy.history("Melding", "Projectmedewerker")
+    cy.history("SIA melding verwerken", "Projectmedewerker")
   })
 })
