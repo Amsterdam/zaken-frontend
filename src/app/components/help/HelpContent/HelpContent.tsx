bff3b4af9decfc570ec57b309ef25878cc491184
remyvdwereld <r.van.der.wereld@amsterdam.nl>
2022-05-12 17:13:16 +0200

    Updated FAQ

--- src/app/components/help/HelpContent/HelpContent.tsx
+++ src/app/components/help/HelpContent/HelpContent.tsx
@@ -1,21 +1,9 @@
-import { useMemo } from "react"
-import { Accordion, Heading, Paragraph, Spinner, themeSpacing } from "@amsterdam/asc-ui"
-
+import { Accordion, Heading, Paragraph, themeSpacing } from "@amsterdam/asc-ui"
 import { RowWithColumn } from "app/components/layouts/Grid"
 import EmailLink from "./EmailLink"
 import PhoneLink from "./PhoneLink"
-
-import { useSupportContacts } from "app/state/rest/"
 import { Refresh } from "app/components/shared/Icons"
 import styled from "styled-components"
-
-const showOptionalProperty = (prop: string | undefined) => prop ?? "(onbekend)"
-
-const mapData = (data: Components.Schemas.PaginatedSupportContactList | undefined) => ([
-  data?.results?.find(({ title }) => title === "Coördinator Ontwikkelkamer"),
-  data?.results?.find(({ title }) => title === "Datapunt"),
-  data?.results?.find(({ title }) => title === "Coördinator AZA")
-])
 
 const RefreshIcon = styled (Refresh)`
   vertical-align: text-top;
@@ -26,67 +14,89 @@ const AccordionWrapper = styled.div`
 `
 
 const HelpContent: React.FC = () => {
-  const [data] = useSupportContacts()
-  const showSpinner = data === undefined
-  const title = "Amsterdamse Zaak Administratie"
+  const title = "AZA/TOP/de Planningstool"
   const titleShort = "AZA"
-  const contacts = useMemo(() => mapData(data), [data])
 
-  return showSpinner ? <Spinner /> : (
+  return (
     <>
       <RowWithColumn>
-        <Heading as="h2">Wat kan je met de { title } ({ titleShort })?</Heading>
-        <Paragraph>
-          De belangrijkste functionaliteiten van { titleShort } staan omschreven in een factsheet.
-        </Paragraph>
-        <Paragraph>
-          Je vindt de factsheet op de G-schijf:<br /><i>G:\WZS\Organisatie\VNH\AZA Werkinstructie</i>
-        </Paragraph>
-      </RowWithColumn>
-      <RowWithColumn>
-        <Heading as="h2">Bij wie kan ik terecht met vragen?</Heading>
+        <Heading as="h2">Waar kan ik terecht met vragen over { title }?</Heading>
       </RowWithColumn>
       <RowWithColumn>
         <AccordionWrapper>
           <Accordion title="Werkproces">
             <Paragraph>
-              Voor het werkproces, oftewel hoe { titleShort } te gebruiken, is een aparte werkinstructie geschreven. Deze kan je opvragen bij je teamleider. De teamleider is er verder ook om vragen en suggesties betreft het werkproces te beantwoorden.
+              Voor afspraken over het werkproces met betrekking tot het gebruik van de applicaties, kan je terecht bij je teamleider. De teamleider is er verder ook om vragen en suggesties betreft het werkproces te beantwoorden.
             </Paragraph>
           </Accordion>
         </AccordionWrapper>
         <AccordionWrapper>
-          <Accordion title="Algemeen gebruik zaaksysteem">
+          <Accordion title="Algemeen gebruik">
             <Paragraph>
-              In de factsheet, waar bovenaan deze pagina naar verwezen wordt, lees je wat je in { titleShort } kan vinden. Heb je specifiekere vragen hoe { titleShort } werkt of kom je ergens niet uit, neem dan contact op met een van de key-users. Dit zijn directe collega’s die veel kennis hebben van { titleShort } en hier speciaal voor getraind zijn. Zij helpen je graag verder. Vraag aan je teamleider bij wie jij het beste terecht kan.
+              De belangrijkste functionaliteiten van { titleShort } staan omschreven in een factsheet.
+            </Paragraph>
+            <Paragraph>
+              Je vindt de factsheet op de G-schijf:<br /><i>G:\WZS\Organisatie\VNH\AZA Werkinstructie</i>
+            </Paragraph>
+            <Paragraph>
+              In de factsheet lees je wat je in { titleShort } kan vinden.
+            </Paragraph>
+            <Paragraph>
+              De handleidingen van TOP/de Planningstool staan op tamtam onder:
+              Wonen - Toezicht & Handhaving - Items
+            </Paragraph>
+            <Paragraph>
+              Heb je specifiekere vragen over hoe de applicaties werken of kom je ergens niet uit, neem dan contact op met een van de key-users. Dit zijn directe collega’s die veel kennis hebben van de applicaties. Zij helpen je graag verder. Vraag aan je teamleider bij wie jij het beste terecht kan.
             </Paragraph>
           </Accordion>
         </AccordionWrapper>
         <AccordionWrapper>
-          <Accordion title="Technisch support" >
+          <Accordion title="Support" >
             <Paragraph>
-              Werkt { titleShort } niet (goed)? Probeer de pagina opnieuw te laden door op het refresh icoon <RefreshIcon /> te klikken.
+              Werkt { title } niet (goed)? Dan kan je de volgende dingen proberen:
+              <ul>
+                <li>De pagina opnieuw te laden door op het refresh icoon <RefreshIcon /> te klikken.</li>
+                <li>Uit te loggen, om vervolgens opnieuw in te loggen.</li>
+              </ul>
             </Paragraph>
             <Paragraph>
-              Werkt { titleShort } dan nog steeds niet, neem dan contact op met:
+              Werkt de applicatie dan nog steeds niet?
             </Paragraph>
-            <ul>
-              <li>Tijdens kantooruren ontwikkelteam { titleShort }, { showOptionalProperty(contacts[0]?.name) } { contacts[0]?.phone_number ? <PhoneLink phoneNumber={ contacts[0].phone_number } /> : showOptionalProperty(undefined) }</li>
-              <li>Buiten kantoortijden Datapunt, { showOptionalProperty(contacts[1]?.name) } { contacts[1]?.phone_number ? <PhoneLink phoneNumber={ contacts[1].phone_number } /> : showOptionalProperty(undefined) }</li>
-            </ul>
+            <Paragraph>
+              Neem dan contact op met:
+              <ul>
+                <li>Binnen kantoortijden: <EmailLink email="ivdesk@amsterdam.nl" /></li>
+                <li>Buiten kantoortijden: <EmailLink email="team.salmagundi.ois@amsterdam.nl" /></li>
+              </ul>
+            </Paragraph>
+            <Paragraph>
+              Neem in de volgende gevallen ook contact op met ivdesk:
+              <ul>
+                <li>Als je problemen hebt bij het inloggen.</li>
+                <li>Er een zaak verwijderd dient te worden.</li>
+                <li>Er een (nieuw) project dient te worden toegevoegd/uitgezet.</li>
+                <li>Er een (nieuw) kenmerk in TOP dient te worden toegevoegd/uitgezet.</li>
+                <li>De gewichten van de variabelen (adres coördinaten/hitkans/prioriteit) in TOP dienen te worden aangepast.</li>
+                <li>Er in een handhavingstraject wordt gevraagd naar het hitkanspercentage van ALPHA in TOP, aangaande het huisbezoek waarbij de constatering is gedaan.</li>
+              </ul>
+
+            </Paragraph>
           </Accordion>
         </AccordionWrapper>
         <AccordionWrapper>
-          <Accordion title={`Feedback ${ titleShort }`}>
+          <Accordion title="Feedback">
             <Paragraph>
-              Ontdek je dingen die niet kloppen of heb je suggesties om { titleShort } nog beter te maken? Dan hoort het ontwikkelteam dit graag. Op elke pagina van { titleShort } zie je aan de rechterkant een rood label: Feedback. Via dit formulier kan je alles aan ons kwijt. We nemen daarna zo snel mogelijk contact met je op om je vragen te beantwoorden.<br/>
+              Ontdek je dingen die niet kloppen of heb je suggesties om AZA nog beter te maken? Op elke pagina van AZA zie je aan de rechterkant een rood label: Feedback. Via dit formulier kan je alles aan ons kwijt. We nemen daarna zo snel mogelijk contact met je op om je vragen te beantwoorden.
             </Paragraph>
             <Paragraph>
-              Liever persoonlijk contact? Neem contact op met { showOptionalProperty(contacts[2]?.name) }, { showOptionalProperty(contacts[2]?.title) }:
+              Liever persoonlijk contact? Of feedback over TOP/de Planningstool?
             </Paragraph>
-            <ul>
-              <li>Telefonisch: te bereiken op ma, di, wo en do tijdens kantooruren op { contacts[2]?.phone_number ? <PhoneLink phoneNumber={ contacts[2].phone_number } /> : showOptionalProperty(undefined) }</li>
-              <li>Per e-mail: { contacts[2]?.email ? <EmailLink email={ contacts[2].email } /> : showOptionalProperty(undefined) }</li>
-            </ul>
+            <Paragraph>
+              Neem dan contact op met: Mèxime Poll.
+              <ul>
+                <li>Ma t/m do tijdens kantooruren op <PhoneLink phoneNumber="06-82686587"/>of <EmailLink email="m.poll@amstedam.nl" /></li>
+              </ul>
+            </Paragraph>
           </Accordion>
         </AccordionWrapper>
       </RowWithColumn>
