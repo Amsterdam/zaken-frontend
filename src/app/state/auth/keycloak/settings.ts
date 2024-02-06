5fabd5e4cd33c704fc33b3a69ecba3c7d6a816cd
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-02-06 10:54:12 +0100

    change clientId

--- src/app/state/auth/keycloak/settings.ts
+++ src/app/state/auth/keycloak/settings.ts
@@ -4,8 +4,8 @@ export default {
   "url": "https://iam.amsterdam.nl/auth/",
   "realm": env.REACT_APP_KEYCLOAK_REALM ?? "",
   "ssl-required": "external",
-  "resource": "wonen-woon-o-azure",
+  "resource": env.REACT_APP_KEYCLOAK_CLIENT_ID ?? "wonen-woon-o-azure",
   "public-client": true,
   "confidential-port": 0,
-  "clientId": "wonen-woon-o-azure"
+  "clientId": env.REACT_APP_KEYCLOAK_CLIENT_ID ?? "wonen-woon-o-azure"
 }
