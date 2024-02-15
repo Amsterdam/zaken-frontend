b7b9c4bc2b7e2f4e3a46cbf670328f1580179ba2
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-02-15 13:52:54 +0100

    merge fixes

--- scripts/src/generateSwaggerSchema.ts
+++ scripts/src/generateSwaggerSchema.ts
@@ -1,22 +1,8 @@
 #!/usr/bin/env ts-node-script
 import { exec } from "child_process"
-<<<<<<< HEAD
-import { config } from "dotenv-flow"
-
-=======
-
-import slashSandwich from "slash-sandwich"
 import { env } from "app/config/env"
->>>>>>> 42bfc866 (Change build to work for multiple environments)
-// Loads .env.development or .env.production based on NODE_ENV
 
-<<<<<<< HEAD
-const url = `${ process.env.REACT_APP_API_HOST }${ process.env.REACT_APP_API_PATH }schema/`
-=======
-
-const url = slashSandwich([env.REACT_APP_API_HOST, env.REACT_APP_API_PATH, "schema"])
->>>>>>> 42bfc866 (Change build to work for multiple environments)
-
+const url = `${ env.REACT_APP_API_HOST }${ env.REACT_APP_API_PATH }schema/`
 exec(`dtsgen -o ./src/__generated__/apiSchema.d.ts --url ${ url }`,
   (error, stdout, stderr) => {
     if (error) {
