1d382f5efc38378bf2575ae94ebac0aa464ba2c1
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2024-07-09 09:52:48 +0200

    Initial commit

---
+++ src/app/state/rest/bagPdok.ts
@@ -0,0 +1,38 @@
+import type { Options } from "."
+import { useErrorHandler } from "./hooks/utils/errorHandler"
+import useApiRequest from "./hooks/useApiRequest"
+import qs from "qs"
+
+const PDOK_URL = "https://api.pdok.nl/bzk/locatieserver/search/v3_1/free"
+// Filters for pdok search api
+const MUNICIPALITY_FILTER = "fq=(gemeentenaam:(amsterdam)) "
+const ADDRESS_FILTER = "AND (type:adres)"
+const ADDRESS_SORT = "straatnaam asc, huisnummer asc, huisletter asc, huisnummertoevoeging asc"
+const DEFAULT_SORT = "score desc, weergavenaam asc"
+const AREA_CODE_FILTER = "AND (type:postcode)"
+
+const FIELD_LIST = "weergavenaam,adrestype,gemeentenaam,nummeraanduiding_id,adresseerbaarobject_id,straatnaam,huisnummer,huisletter,huisnummertoevoeging,postcode,woonplaatsnaam,centroide_ll,score"
+const START = 0
+const RESULTS_PER_PAGE = 20
+
+
+export const useBagPdok = (searchString?: string, options?: Options) => {
+  const handleError = useErrorHandler()
+  const query = qs.stringify({ 
+    q: searchString,
+    fq: `${ MUNICIPALITY_FILTER }${ ADDRESS_FILTER }`,
+    fl: FIELD_LIST,
+    start: START.toString(),
+    rows: RESULTS_PER_PAGE.toString(),
+    sort: DEFAULT_SORT
+  }, { 
+    addQueryPrefix: true 
+  })
+  return useApiRequest<BAGPdokResponse>({
+    url: `${ PDOK_URL }${ query }`,
+    lazy: searchString === undefined,
+    ...options,
+    groupName: "dataPunt",
+    handleError
+  })
+}
