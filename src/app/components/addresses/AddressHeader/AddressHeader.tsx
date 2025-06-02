3e08c56c699211252c7c2baa0487fbc6c6f4d28c
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-06-02 23:27:14 +0200

    WON-228 Add Alert message for missing brk data

--- src/app/components/addresses/AddressHeader/AddressHeader.tsx
+++ src/app/components/addresses/AddressHeader/AddressHeader.tsx
@@ -1,12 +1,16 @@
-import styled from "styled-components"
-import { breakpoint, themeSpacing, Typography } from "@amsterdam/asc-ui"
+import { useEffect } from "react"
+import { Typography } from "@amsterdam/asc-ui"
 import { SmallSkeleton } from "@amsterdam/wonen-ui"
 
 import { useBagPdokByBagId } from "app/state/rest"
-import ShowOtherAddressesButton, { Index } from "app/components/addresses/AddressSuffixSwitcher/ShowOtherAddressesButton"
+import ShowOtherAddressesButton, {
+  Index
+} from "app/components/addresses/AddressSuffixSwitcher/ShowOtherAddressesButton"
 import useOtherAddressesByBagId from "app/state/rest/custom/useOtherAddresses/useOtherAddresses"
 import AddressLink from "./components/AddressLink"
 import { getAddressFromBagPdokResponse } from "app/components/addresses/utils"
+import { useFlashMessages } from "app/state/flashMessages/useFlashMessages"
+import styles from "./AddressHeader.module.css"
 
 type Props = {
   bagId: Components.Schemas.Address["bag_id"]
@@ -15,44 +19,50 @@ type Props = {
   enableSwitch?: boolean
 }
 
-const Div = styled.div<{ isHeader: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  @media screen and ${ breakpoint("min-width", "laptopM") } {
-    justify-content: ${ props => props.isHeader ? "flex-start" : "flex-end" };
-  }
-`
-
-const ButtonWrap = styled.div`
-  margin: -${ themeSpacing(2) } 0 0 ${ themeSpacing(3) };
-`
-
-const AddressHeader: React.FC<Props> = ({ bagId, headingSize = "h2", isHeader = false, enableSwitch = true }) => {
+const AddressHeader: React.FC<Props> = ({
+  bagId,
+  headingSize = "h2",
+  isHeader = false,
+  enableSwitch = true
+}) => {
   const [data, { isBusy }] = useBagPdokByBagId(bagId)
   const foundAddress = getAddressFromBagPdokResponse(data)
   const [filteredAddresses] = useOtherAddressesByBagId(bagId)
+  const { addErrorFlashMessage } = useFlashMessages()
   const showButton = enableSwitch && (filteredAddresses?.length ?? 0) > 1
-  const isCurrentAddress = (address: BAGPdokAddress ) => address.weergavenaam === foundAddress?.weergavenaam
+  const isCurrentAddress = (address: BAGPdokAddress) =>
+    address.weergavenaam === foundAddress?.weergavenaam
   const addressIndex = filteredAddresses?.findIndex(isCurrentAddress) ?? -1
   let index: Index = undefined
   if (addressIndex === 0) {
     index = "first"
-  } else if (addressIndex > 0 && addressIndex === (filteredAddresses?.length ?? 0) - 1) {
+  } else if (
+    addressIndex > 0 &&
+    addressIndex === (filteredAddresses?.length ?? 0) - 1
+  ) {
     index = "last"
   }
 
+  useEffect(() => {
+    if (!isBusy && data?.response?.docs?.length === 0) {
+      addErrorFlashMessage(
+        "Oeps er ging iets mis!",
+        "Het ophalen van de BAG-informatie uit het BRK is mislukt. Zijn de adresgegevens gewijzigd? Maak een melding via de feedbackknop."
+      )
+    }
+  }, [addErrorFlashMessage, data?.response?.docs, isBusy])
+
   const title = foundAddress?.weergavenaam
+  const className = isHeader ? styles.header : styles.default
+
   return (
-    <Div isHeader={ isHeader }>
-      { isBusy && <SmallSkeleton height={ 10 } /> }
-      { title && <AddressLink title={ title } bagId={ bagId } as={ headingSize ?? "span" } /> }
-      { showButton && (
-        <ButtonWrap>
-          <ShowOtherAddressesButton bagId={ bagId } index={ index } />
-        </ButtonWrap>
+    <div className={className}>
+      {isBusy && <SmallSkeleton height={10} />}
+      {title && (
+        <AddressLink title={title} bagId={bagId} as={headingSize ?? "span"} />
       )}
-    </Div>
+      {showButton && <ShowOtherAddressesButton bagId={bagId} index={index} />}
+    </div>
   )
 }
 export default AddressHeader
