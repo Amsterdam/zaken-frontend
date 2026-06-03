a44622407c226bbfcf683c430185778ed26484e2
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2026-06-03 12:43:10 +0200

    Refactor: Update FilterSearch and MultipleOptionsFilterBox components for improved code consistency and readability

--- src/app/components/filters/MultipleOptionsFilterBox/MultipleOptionsFilterBox.tsx
+++ src/app/components/filters/MultipleOptionsFilterBox/MultipleOptionsFilterBox.tsx
@@ -4,22 +4,30 @@ import FilterCard from "./FilterCard";
 import FilterSearch from "./FilterSearch";
 import { FilterWrapper, StyledLabel } from "../FilterStyle";
 
-export type Option = { 
-  id?: number | string
-  name: string 
-}
+export type Option = {
+  id?: number | string;
+  name: string;
+};
 
 type Props = {
-  label: string
-  options?: Option[]
-  selectedOptions: string[]
-  setSelectedOptions: (value: string[]) => void
-  byId?: boolean
-}
+  label: string;
+  options?: Option[];
+  selectedOptions: string[];
+  setSelectedOptions: (value: string[]) => void;
+  byId?: boolean;
+};
 
-const MultipleOptionsFilterBox: React.FC<Props> = ({ label, options, selectedOptions, setSelectedOptions, byId = false }) => {
+const MultipleOptionsFilterBox: React.FC<Props> = ({
+  label,
+  options,
+  selectedOptions,
+  setSelectedOptions,
+  byId = false,
+}) => {
   const [isFocussed, setIsFocussed] = useState(false);
-  const [searchedOptions, setSearchedOptions] = useState<Option[] | undefined>(undefined);
+  const [searchedOptions, setSearchedOptions] = useState<Option[] | undefined>(
+    undefined,
+  );
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { checked, value } = e.target;
@@ -27,7 +35,9 @@ const MultipleOptionsFilterBox: React.FC<Props> = ({ label, options, selectedOpt
     if (checked) {
       newSelectedOptions.push(value);
     } else {
-      newSelectedOptions = selectedOptions.filter((selectedOption) => selectedOption !== value);
+      newSelectedOptions = selectedOptions.filter(
+        (selectedOption) => selectedOption !== value,
+      );
     }
     setSelectedOptions(newSelectedOptions);
   };
@@ -35,7 +45,9 @@ const MultipleOptionsFilterBox: React.FC<Props> = ({ label, options, selectedOpt
   const onChangeFilterSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const searchString = e.target.value;
     if (searchString) {
-      const result = options?.filter((s) => s.name.toLocaleLowerCase().includes(searchString.toLocaleLowerCase()));
+      const result = options?.filter((s) =>
+        s.name.toLocaleLowerCase().includes(searchString.toLocaleLowerCase()),
+      );
       setSearchedOptions(result);
     } else {
       setSearchedOptions(undefined);
@@ -46,35 +58,35 @@ const MultipleOptionsFilterBox: React.FC<Props> = ({ label, options, selectedOpt
   return (
     <>
       <FilterWrapper>
-        <StyledLabel label={ label } />
+        <StyledLabel label={label} />
         <FilterSearch
-          onChange={ onChangeFilterSearch }
-          onFocus={ () => setIsFocussed(true) }
-          label={ label }
+          onChange={onChangeFilterSearch}
+          onFocus={() => setIsFocussed(true)}
         />
         <FilterCard
-          isVisible={ isFocussed || selectedOptions.length > 0 }
-          hasItems={ !!(visibleOptions && visibleOptions?.length > 0) }
+          isVisible={isFocussed || selectedOptions.length > 0}
+          hasItems={!!(visibleOptions && visibleOptions?.length > 0)}
         >
-          { visibleOptions?.map((option) => {
-            const value = byId && option?.id ? option?.id?.toString() : option.name;
+          {visibleOptions?.map((option) => {
+            const value =
+              byId && option?.id ? option?.id?.toString() : option.name;
             return (
               <Label
-                htmlFor={ option.name }
-                label={ option.name }
-                key={ option.name }
-                style={ { width: "100%" } }
+                htmlFor={option.name}
+                label={option.name}
+                key={option.name}
+                style={{ width: "100%" }}
               >
                 <Checkbox
-                  id={ option.name }
-                  data-testid={ option.name }
-                  defaultValue={ value }
-                  onChange={ onChange }
-                  checked={ selectedOptions.includes(value) }
+                  id={option.name}
+                  data-testid={option.name}
+                  defaultValue={value}
+                  onChange={onChange}
+                  checked={selectedOptions.includes(value)}
                 />
               </Label>
-            ); })
-          }
+            );
+          })}
         </FilterCard>
       </FilterWrapper>
     </>
